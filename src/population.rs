35cfb58ffe8b172f3fc9525ad1018c16289331c3
Nic McPhee <mcphee@morris.umn.edu>
2022-09-21 21:56:11 -0500

    Remove the `ParentSelector` type
    That logic has all been subsumed by the new `Generation` type, so I've removed all that stuff from `population.rs`.

--- src/population.rs
+++ src/population.rs
@@ -73,45 +73,6 @@ impl<T> Population<T> {
 }
 
 pub type Selector<T> = dyn Fn(&Population<T>) -> Option<&Individual<T>> + Sync + Send;
-
-// TODO: Should this just become part of the `Population` type?
-//   We could provide a set of selectors in the constructor (or
-//   a builder) for `Population`, and then just have a `get_parent()`
-//   method there.
-// TODO: Extend this to a WeightedParentSelector that is essentially
-//   a wrapper around `rand::distributions::WeightedChoice` so we can
-//   provide weights on the different selectors.
-pub struct ParentSelector<'a, T> {
-    population: &'a Population<T>,
-    selectors: &'a Vec<&'a Selector<T>>,
-}
-
-impl<'a, T> ParentSelector<'a, T> {
-    fn new(population: &'a Population<T>, selectors: &'a Vec<&'a Selector<T>>) -> Self {
-        ParentSelector {
-            population,
-            selectors
-        }
-    }
-
-    pub fn get(&self, rng: &mut ThreadRng) -> Option<&'a Individual<T>> {
-        let s = self.selectors.choose(rng)?;
-        s(self.population)
-    }
-}
-
-impl<T> Population<T> {
-    // TODO: This iterator is sequential, and we might want it to become parallel,
-    // if that is feasible. I'll need to better understand how to implement Rayon's
-    // parallel iterator trait.
-    #[must_use]
-    pub fn parent_selector<'a>(&'a self, selectors: &'a Vec<&'a Selector<T>>) -> ParentSelector<T> {
-        // TODO: We might want or need to move this somewhere else if we convert
-        // to parallel iterators.
-        ParentSelector::new(self, selectors)
-    }
-}
-
 impl<T> Population<T> {
     /// # Panics
     ///
