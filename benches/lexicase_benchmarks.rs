03ad046fbef0e16f7a446d73b6c0bf6df604a572
Nic McPhee <mcphee@morris.umn.edu>
2022-10-26 18:57:37 -0500

    Finish implementing lexicase benchmarks
    This wraps up a reasonable version of these, although they take a while to run.

--- benches/lexicase_benchmarks.rs
+++ benches/lexicase_benchmarks.rs
@@ -1,11 +1,31 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
-use rust_ga::{bitstring::{Bitstring, hiff}, population::Population};
+use std::iter;
+
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use rust_ga::{bitstring::{Bitstring, hiff}, population::Population, individual::Individual};
+
+fn make_uniform_pop() -> Population<Bitstring> {
+    let ind = Individual::new_bitstring(128, hiff, &mut rand::thread_rng());
+    let individuals = iter::repeat(ind).take(1000).collect();
+    Population { individuals }
+}
+
+fn make_num_distinct_pop(num_distinct_groups: usize) -> Population<Bitstring> {
+    let group_size = 1000 / num_distinct_groups;
+    let mut individuals = Vec::new();
+    for _ in 0..num_distinct_groups {
+        let ind = Individual::new_bitstring(128, hiff, &mut rand::thread_rng());
+        for _ in 0..group_size {
+            individuals.push(ind.clone());
+        }
+    }
+    Population { individuals }
+}
 
 fn make_benchmark_populations() -> Vec<(Population<Bitstring>, String)> {
     vec![
-        (Population::new_bitstring_population(1000, 128, hiff), "Random".to_string())
+        (Population::new_bitstring_population(1000, 128, hiff), "Random".to_string()),
+        (make_uniform_pop(), "Uniform".to_string())
     ]
-    // todo!()
 }
 
 fn bench_lexicase(c: &mut Criterion) {
@@ -14,6 +34,13 @@ fn bench_lexicase(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("simple_lexicase", pop_name), population, 
             |b, p| b.iter(|| p.simple_lexicase()));
         group.bench_with_input(BenchmarkId::new("lexicase_with_dup_removal", pop_name), population, 
+            |b, p| b.iter(|| p.lexicase_with_dup_removal()));
+    }
+    for num_groups in [10, 20, 25, 40, 50, 100, 200, 500, 1000] {
+        let population = make_num_distinct_pop(num_groups);
+        group.bench_with_input(BenchmarkId::new("simple_lexicase", num_groups), &population, 
+            |b, p| b.iter(|| p.simple_lexicase()));
+        group.bench_with_input(BenchmarkId::new("lexicase_with_dup_removal", num_groups), &population, 
             |b, p| b.iter(|| p.lexicase_with_dup_removal()));
     }
     group.finish();
