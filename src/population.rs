996e324d4065fb7b39de75a40bba64f0cc4f616f
Nic McPhee <mcphee@morris.umn.edu>
2022-10-26 22:36:31 -0500

    Add two new versions of lexicase selection
    `one_pass_lexicase` tries to translate the approach taken in Ellen over here to Rust. There are actually two versions of that; one uses an explicit for-loop and the other uses `fold()`, but it didn't make a bit difference to the performance.
    
    `reuse_vector_lexicase` is similar to the previous one, but uses some nifty tricks (thanks to esitsu@Twitch for the `swap` idea!) to minimize the allocation and deallocation.

--- src/population.rs
+++ src/population.rs
@@ -3,7 +3,7 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use std::borrow::Borrow;
+use std::{borrow::Borrow, mem::swap};
 use itertools::Itertools;
 use rand::seq::SliceRandom;
 
@@ -101,6 +101,7 @@ impl<T> Population<T> {
             .max_by_key(|ind| ind.total_score)
     }
 
+    // TODO: Why are we returning `Option`s for all the selectors?
     pub fn simple_lexicase(&self) -> Option<&Individual<T>> {
         // Candidate set is initially the whole population.
         // Shuffle the (indices of the) test cases.
@@ -139,6 +140,7 @@ impl<T> Population<T> {
             }).collect();
         }
 
+        candidates.shuffle(&mut rand::thread_rng());
         Some(candidates[0])
     }
 
@@ -178,6 +180,115 @@ impl<T> Population<T> {
             }).collect();
         }
 
+        candidates.shuffle(&mut rand::thread_rng());
+        Some(candidates[0])
+    }
+
+    pub fn one_pass_lexicase(&self) -> Option<&Individual<T>> {
+        // Candidate set is initially the whole population.
+        // Shuffle the (indices of the) test cases.
+        // For each test in turn:
+        //   * Find the best score of any individual still in
+        //     in the candidate set on that test case.
+        //   * Remove any individual from the candidate set that
+        //     is worse than that best score on that test case.
+        // Go until you get to a single individual or you run
+        // out of test cases.
+
+        // TODO: Compute these bits once when the population is initially constructed
+        //   and then just look it up when necessary instead of recomputing it
+        //   for every selection.
+        let first_individual = &self.individuals[0];
+        let first_scores = &first_individual.scores;
+        let num_scores = first_scores.len();
+        let mut case_indices: Vec<usize> = (0..num_scores).collect();
+
+        case_indices.shuffle(&mut rand::thread_rng());
+
+        let mut candidates: Vec<&Individual<T>> = self.individuals
+            .iter()
+            .collect();
+
+        for test_case_index in case_indices {
+            assert!(!candidates.is_empty(), "The set of lexicase candidates shouldn't be empty");
+            if candidates.len() == 1 {
+                break;
+            }
+            // let mut winners = vec![candidates[0]];
+            // for c in &candidates[1..] {
+            //     if c.scores[test_case_index] > winners[0].scores[test_case_index] {
+            //         winners = Vec::with_capacity(candidates.len());
+            //         winners.push(c);
+            //     } else if c.scores[test_case_index] == winners[0].scores[test_case_index] {
+            //         winners.push(c);
+            //     }
+            // }
+            // candidates = winners;
+            let mut winners = Vec::with_capacity(candidates.len());
+            winners.push(candidates[0]);
+            candidates = candidates
+                .into_iter()
+                .skip(1)
+                .fold(winners, |mut winners, individual| {
+                    if individual.scores[test_case_index] > winners[0].scores[test_case_index] {
+                        winners.clear();
+                        winners.push(individual);
+                    } else if individual.scores[test_case_index] == winners[0].scores[test_case_index] {
+                        winners.push(individual);
+                    }
+                    winners
+                });
+        }
+
+        candidates.shuffle(&mut rand::thread_rng());
+        Some(candidates[0])
+    }
+
+    pub fn reuse_vector_lexicase(&self) -> Option<&Individual<T>> {
+        // Candidate set is initially the whole population.
+        // Shuffle the (indices of the) test cases.
+        // For each test in turn:
+        //   * Find the best score of any individual still in
+        //     in the candidate set on that test case.
+        //   * Remove any individual from the candidate set that
+        //     is worse than that best score on that test case.
+        // Go until you get to a single individual or you run
+        // out of test cases.
+
+        // TODO: Compute these bits once when the population is initially constructed
+        //   and then just look it up when necessary instead of recomputing it
+        //   for every selection.
+        let first_individual = &self.individuals[0];
+        let first_scores = &first_individual.scores;
+        let num_scores = first_scores.len();
+        let mut case_indices: Vec<usize> = (0..num_scores).collect();
+
+        case_indices.shuffle(&mut rand::thread_rng());
+
+        let mut candidates: Vec<&Individual<T>> = self.individuals
+            .iter()
+            .collect();
+
+        let mut winners = Vec::with_capacity(candidates.len());
+        for test_case_index in case_indices {
+            assert!(!candidates.is_empty(), "The set of lexicase candidates shouldn't be empty");
+            if candidates.len() == 1 {
+                break;
+            }
+            winners.clear();
+            winners.push(candidates[0]);
+            for c in &candidates[1..] {
+                if c.scores[test_case_index] > winners[0].scores[test_case_index] {
+                    winners.clear();
+                    winners.push(c);
+                } else if c.scores[test_case_index] == winners[0].scores[test_case_index] {
+                    winners.push(c);
+                }
+            }
+            swap(&mut candidates, &mut winners);
+        }
+
+        candidates.shuffle(&mut rand::thread_rng());
         Some(candidates[0])
     }
 }
