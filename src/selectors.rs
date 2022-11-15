f831030d82aad111f3ab9891b6d79964fb9e72ca
Nic McPhee <mcphee@morris.umn.edu>
2022-11-15 12:58:16 -0600

    Implement `Weighted` composite selector
    This replaces the old `weighted_selector` field type with a new composite implementation of the `Selector` trait. This lets us either give things like `Generation` either a "simple" selector (like `Lexicase`) or a composite selector (e.g., `Weighted`), and nothing has to change in `Generation`.
    
    We also used the builder pattern for `Weighted`. Since the collection of selectors should never be empty, I implemented `new()` to take a single selector-weight pair, and then `with_selector` allows us to add additional selector-weight pairs.
    
    This was one of the high points of today's stream and worked beautifully!

--- src/selectors.rs
+++ src/selectors.rs
@@ -124,3 +124,35 @@ impl<G, R: Ord> Selector<G, TestResults<R>> for Lexicase {
         candidates[0]
     }
 }
+
+pub struct Weighted<'a, G, R> {
+    selectors: Vec<(&'a dyn Selector<G, R>, usize)>
+}
+
+impl<'a, G, R> Weighted<'a, G, R> {
+    // Since we should never have an empty collection of weighted selectors,
+    // the `new` implementation takes an initial selector so `selectors` is
+    // guaranteed to never be empty.
+    #[must_use]
+    pub fn new(selector: &'a dyn Selector<G, R>, weight: usize) -> Self {
+        Self { selectors: vec![ (selector, weight) ] }
+    }
+
+    #[must_use]
+    pub fn with_selector(mut self, selector: &'a dyn Selector<G, R>, weight: usize) -> Self {
+        self.selectors.push((selector, weight));
+        self
+    }
+}
+
+impl<'a, G, R> Selector<G, R> for Weighted<'a, G, R> {
+    fn select<'b>(&self, rng: &mut ThreadRng, population: &'b Population<G, R>) -> &'b Individual<G, R> {
+        assert!(self.selectors.is_empty().not(), "The collection of selectors should be non-empty");
+        #[allow(clippy::unwrap_used)]
+        let (selector, _) = self
+            .selectors
+            .choose_weighted(rng, |(_, w)| *w)
+            .unwrap();
+        selector.select(rng, population)
+    }
+}
