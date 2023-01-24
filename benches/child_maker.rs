f5f165f707200004d2ef4cd95bd488863ac195bb
Nic McPhee <mcphee@morris.umn.edu>
2023-01-24 10:22:23 -0600

    Move population construction out of `child_maker` benchmark
    This was also benchmarking the construction of the population, which wasn't appropriate.
    
    While I was here I also black-boxed the arguments to `make_child`. I don't know if that actually mattered, but I doubt it hurt.

--- benches/child_maker.rs
+++ benches/child_maker.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, black_box};
 use rust_ga::{
     bitstring::new_bitstring_population,
     child_maker::{two_point_xo_mutate::TwoPointXoMutate, ChildMaker},
@@ -21,16 +21,18 @@ fn child_maker(c: &mut Criterion) {
         scorer: &trivial_scorer,
     };
 
+    let population = new_bitstring_population(
+        1,
+        16,
+        // TODO: I should really have a function somewhere that converts functions
+        //   that return vectors of scores to `TestResults` structs.
+        trivial_tester,
+    );
+
     c.bench_function("Two-point Xo + mutate", |b| {
         b.iter(|| {
-            let population = new_bitstring_population(
-                1,
-                128,
-                // TODO: I should really have a function somewhere that converts functions
-                //   that return vectors of scores to `TestResults` structs.
-                trivial_tester,
-            );
-            let child = two_point_xo_mutate.make_child(&mut rng, &population, &Best);
+            let child 
+                = two_point_xo_mutate.make_child(black_box(&mut rng), black_box(&population), black_box(&Best));
             assert_eq!(0, child.test_results().total_result);
         })
     });
