4600d288a158e4f4b16dca49da357fab623d2c93
Nic McPhee <mcphee@morris.umn.edu>
2023-01-23 21:06:14 -0600

    Reformat the `child_maker` benchmark

--- benches/child_maker.rs
+++ benches/child_maker.rs
@@ -1,26 +1,20 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rust_ga::{
-    bitstring::{new_bitstring_population},
-    child_maker::{
-        two_point_xo_mutate::{TwoPointXoMutate},
-        ChildMaker,
-    },
+    bitstring::new_bitstring_population,
+    child_maker::{two_point_xo_mutate::TwoPointXoMutate, ChildMaker},
+    individual::Individual,
     selector::best::Best,
-    test_results::TestResults, individual::Individual,
+    test_results::TestResults,
 };
 
 fn child_maker(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
 
-    let trivial_scorer = |_: &[bool]| {
-        vec![0i64]
-    };
+    let trivial_scorer = |_: &[bool]| vec![0i64];
 
-    let trivial_tester = |_: &Vec<bool>| {
-        TestResults {
-            total_result: 0i64,
-            results: vec![0i64] 
-        }
+    let trivial_tester = |_: &Vec<bool>| TestResults {
+        total_result: 0i64,
+        results: vec![0i64],
     };
 
     let two_point_xo_mutate = TwoPointXoMutate {
@@ -34,10 +28,9 @@ fn child_maker(c: &mut Criterion) {
                 128,
                 // TODO: I should really have a function somewhere that converts functions
                 //   that return vectors of scores to `TestResults` structs.
-                trivial_tester
+                trivial_tester,
             );
-            let child 
-                = two_point_xo_mutate.make_child(&mut rng, &population, &Best);
+            let child = two_point_xo_mutate.make_child(&mut rng, &population, &Best);
             assert_eq!(0, child.test_results().total_result);
         })
     });
