2efaff8c6bb6d4b897b1ded799f55e53155e74b4
Nic McPhee <mcphee@morris.umn.edu>
2023-02-12 11:52:47 -0600

    Move `Sized` into `then` in `Composable`
    Instead of making all implementations of `Composable` implement `Sized`, this moved the `Sized` requirement into the function so it only is checked/required when we use the `then` function.
    
    This is related to the whole object safety thing that I don't understand very well and need to do some homework on.

--- src/operator/composable.rs
+++ src/operator/composable.rs
@@ -8,8 +8,15 @@ mod then;
 // blanket implementation down the road, but I was to see why we need it.
 // impl<T> Composable for T {}
 
-pub trait Composable: Sized {
-    fn then<Op>(self, op: Op) -> Then<Self, Op> {
+// TODO: Do some homework on the role of `Sized` here. It used to be
+//  on the trait `Composable`, but moving into the function fixed
+//  some things in `Weighted`. This is related to the whole object
+//  safety thing that I don't understand terribly well.
+pub trait Composable {
+    fn then<Op>(self, op: Op) -> Then<Self, Op> 
+    where
+        Self: Sized
+    {
         Then::new(self, op)
     }
 
