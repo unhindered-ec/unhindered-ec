dcb78079331e05a2b4cb0ab9d24e3f565c72e6a4
Nic McPhee <mcphee@morris.umn.edu>
2023-02-12 11:56:59 -0600

    Remove all the references to `Selector` in `Weighted`
    This changes the vector of selectors to be impls of the `Operator` trait instead of the `Selector` trait, and removes all references to `Selector` except for the one `impl Selector` that we'll ultimately remove.
    
    When we do remove that, we should be able to simplify some of the existing code (e..g, removing the `'pop` lifetime in `SelectorOperator`, and make more general.

--- src/selector/weighted.rs
+++ src/selector/weighted.rs
@@ -9,11 +9,16 @@ use crate::{
 
 use super::Selector;
 
-pub struct Weighted<'sel, P> {
-    selectors: Vec<(&'sel dyn Selector<P>, usize)>,
+type SelectorOperator<'sel, P> =
+    &'sel (dyn for<'pop> Operator<&'pop P, Output = &'pop <P as Population>::Individual> + Sync);
+
+// TODO: When we remove the `Selector`, we can simplify this a lot, removing
+//   the `'pop` lifetime and making it more generic.
+pub struct Weighted<'sel, P: Population> {
+    selectors: Vec<(SelectorOperator<'sel, P>, usize)>,
 }
 
-impl<'sel, P> Clone for Weighted<'sel, P> {
+impl<'sel, P: Population> Clone for Weighted<'sel, P> {
     fn clone(&self) -> Self {
         Self {
             selectors: self.selectors.clone(),
@@ -21,19 +26,19 @@ impl<'sel, P> Clone for Weighted<'sel, P> {
     }
 }
 
-impl<'sel, P> Weighted<'sel, P> {
+impl<'sel, P: Population> Weighted<'sel, P> {
     // Since we should never have an empty collection of weighted selectors,
     // the `new` implementation takes an initial selector so `selectors` is
     // guaranteed to never be empty.
     #[must_use]
-    pub fn new(selector: &'sel dyn Selector<P>, weight: usize) -> Self {
+    pub fn new(selector: SelectorOperator<'sel, P>, weight: usize) -> Self {
         Self {
             selectors: vec![(selector, weight)],
         }
     }
 
     #[must_use]
-    pub fn with_selector(mut self, selector: &'sel dyn Selector<P>, weight: usize) -> Self {
+    pub fn with_selector(mut self, selector: SelectorOperator<'sel, P>, weight: usize) -> Self {
         self.selectors.push((selector, weight));
         self
     }
@@ -42,13 +47,6 @@ impl<'sel, P> Weighted<'sel, P> {
 impl<'sel, P: Population> Selector<P> for Weighted<'sel, P> {
     fn select<'pop>(&self, rng: &mut ThreadRng, population: &'pop P) -> &'pop P::Individual {
         self.apply(population, rng)
-        // assert!(
-        //     self.selectors.is_empty().not(),
-        //     "The collection of selectors should be non-empty"
-        // );
-        // #[allow(clippy::unwrap_used)]
-        // let (selector, _) = self.selectors.choose_weighted(rng, |(_, w)| *w).unwrap();
-        // selector.select(rng, population)
     }
 }
 
@@ -65,7 +63,7 @@ where
         );
         #[allow(clippy::unwrap_used)]
         let (selector, _) = self.selectors.choose_weighted(rng, |(_, w)| *w).unwrap();
-        selector.select(rng, population)
+        selector.apply(population, rng)
     }
 }
-impl<'sel, P> Composable for Weighted<'sel, P> {}
+impl<'sel, P: Population> Composable for Weighted<'sel, P> {}
