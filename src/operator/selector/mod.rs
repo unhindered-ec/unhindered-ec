2431bc527b186fc64eff044547e113443bcad9a8
Nic McPhee <mcphee@morris.umn.edu>
2023-02-26 12:44:26 -0600

    Added esitsu's wrapper type for `Selector`
    I really wanted to have a name for selection operators instead of just having the huge generics. We couldn't do that with a `Selector` trait, which brought us back (after many weeks) to esitsu@Twitch's (now old) suggestion to have wrapper types to allow us to move from `Selector` to `Operator`.
    
    So here we have a new `Selector` trait and specifies a `select` method. We then make a wrapper `struct Select` that implements the `Operator` trait which is specialized for selection. This allows us to encapsulate all the weird generics in `Select`, and also `impl Composable for Select` so we don't have to `impl Composable` for all the specific selectors.

--- src/operator/selector/mod.rs
+++ src/operator/selector/mod.rs
@@ -1,14 +1,41 @@
+use rand::rngs::ThreadRng;
+
+use crate::population::Population;
+
+use super::{Composable, Operator};
+
 pub mod best;
 pub mod lexicase;
 pub mod random;
 pub mod tournament;
 pub mod weighted;
 
-// TODO: We could explore named sub-traits or wrapper types later if
-//   we feel they would be helpful.
-// trait SelectionOp<P>
-// where
-//     Self: for<'pop> Operator<&'pop P, Output = &'pop P::Individual>,
-//     P: Population,
-// {
-// }
+pub trait Selector<P>
+where
+    P: Population,
+{
+    fn select<'pop>(&self, population: &'pop P, rng: &mut ThreadRng) -> &'pop P::Individual;
+}
+
+pub struct Select<S> {
+    selector: S,
+}
+
+impl<S> Select<S> {
+    pub const fn new(selector: S) -> Self {
+        Self { selector }
+    }
+}
+
+impl<'pop, P, S> Operator<&'pop P> for Select<S>
+where
+    P: Population,
+    S: Selector<P>,
+{
+    type Output = &'pop P::Individual;
+
+    fn apply(&self, population: &'pop P, rng: &mut ThreadRng) -> Self::Output {
+        self.selector.select(population, rng)
+    }
+}
+impl<S> Composable for Select<S> {}
