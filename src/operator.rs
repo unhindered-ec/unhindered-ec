cc001d70212f638c190854a3e76bc1bf2591658f
Nic McPhee <mcphee@morris.umn.edu>
2023-02-26 12:38:14 -0600

    Add an `impl` that "converts" `&Operator` to `Operator`
    There ended up being places where we had references to operators, and we wanted to be able to just call `apply()` on those, so we added `impl Operator for &Operator`.

--- src/operator.rs
+++ src/operator.rs
@@ -1,8 +1,8 @@
 use rand::rngs::ThreadRng;
 
 mod composable;
-pub mod selector;
 pub mod recombinator;
+pub mod selector;
 
 pub use composable::Composable;
 
@@ -11,3 +11,16 @@ pub trait Operator<Input>: Composable {
 
     fn apply(&self, input: Input, rng: &mut ThreadRng) -> Self::Output;
 }
+
+impl<T, Input> Operator<Input> for &T
+where
+    T: Operator<Input>,
+{
+    type Output = T::Output;
+
+    fn apply(&self, input: Input, rng: &mut ThreadRng) -> Self::Output {
+        (*self).apply(input, rng)
+    }
+}
+
+impl<T> Composable for &T where T: Composable {}
