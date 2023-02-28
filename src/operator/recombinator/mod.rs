6f0c0048277139c69fbc9f5915ee84a26c796d74
Nic McPhee <mcphee@morris.umn.edu>
2023-02-27 22:00:13 -0600

    Add `Mutator` and `Mutate`
    These are similar to the `Selector` and `Select` operators.
    
    I also started creating a binary (crossover) recombinator `Binary`, but that hasn't been finished yet.

--- src/operator/recombinator/mod.rs
+++ src/operator/recombinator/mod.rs
@@ -1,3 +1,7 @@
+use rand::rngs::ThreadRng;
+
+use super::{Operator, Composable};
+
 pub mod mutate_with_one_over_length;
 pub mod mutate_with_rate;
 pub mod two_point_xo;
@@ -7,3 +11,33 @@ pub mod uniform_xo;
 //   crossovers into `operator::crossover`, and simplify
 //   the type names to not repeat "mutate_with" and "xo"
 //   everywhere.
+
+pub trait Mutator<G> {
+    fn mutate(&self, genome: G, rng: &mut ThreadRng) -> G;
+}
+
+pub struct Mutate<M> {
+    mutator: M,
+}
+
+impl<M> Mutate<M> {
+    pub const fn new(mutator: M) -> Self {
+        Self { mutator }
+    }
+}
+
+impl<M, G> Operator<G> for Mutate<M>
+where
+    M: Mutator<G>
+{
+    type Output = G;
+
+    fn apply(&self, genome: G, rng: &mut ThreadRng) -> Self::Output {
+        self.mutator.mutate(genome, rng)
+    }
+}
+impl<M> Composable for Mutate<M> {}
+
+pub trait Binary<G> {
+    fn recombine(&self, genomes: [G; 2], rng: &mut ThreadRng) -> G;
+}
