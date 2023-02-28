788433e10d928b8e8ee4ed5215ceba51e5dfd998
Nic McPhee <mcphee@morris.umn.edu>
2023-02-28 11:51:07 -0600

    Implement `And` composition operator
    This implements `And` in a way that's quite similar to `Then`, but returns a pair of results instead of composing the two operators.

--- src/operator/composable.rs
+++ src/operator/composable.rs
@@ -1,6 +1,7 @@
-use self::then::Then;
+use self::{then::Then, and::And};
 
 mod then;
+mod and;
 
 // TODO: Rationalize the naming of module files. I think I want
 //   to go back to using `mod.rs` inside sub-folders. At a minimum
@@ -13,9 +14,12 @@ pub trait Composable {
         Then::new(self, op)
     }
 
-    // fn then_mutate<M>(self, mutator: M) -> Then<Self, Mutate<M>> {
-    //     Then::new(self, Mutate::new(mutator))
-    // }
+    fn and<Op>(self, op: Op) -> And<Self, Op>
+    where
+        Self: Sized,
+    {
+        And::new(self, op)
+    }
 
     // fn and_select<S>(self, selector: S) -> Then<Self, Select<S>> {
     //     Then::new(self, Select::new(selector))
