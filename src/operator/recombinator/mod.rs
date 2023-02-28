58dddfe3495448d900d7f2d7ab36f23a63d167c4
Nic McPhee <mcphee@morris.umn.edu>
2023-02-28 10:31:03 -0600

    Add trait & wrapper for recombination
    esitsu@Twitch pointed out that I could generalize my `Binary` trait by not specifying the input type (that can be specified when we `impl` the trait), and using an associated type to capture the output type.
    
    This leads to a `Recombine` that is very similar to the `Operator` trait.

--- src/operator/recombinator/mod.rs
+++ src/operator/recombinator/mod.rs
@@ -38,6 +38,30 @@ where
 }
 impl<M> Composable for Mutate<M> {}
 
-pub trait Binary<G> {
-    fn recombine(&self, genomes: [G; 2], rng: &mut ThreadRng) -> G;
+pub trait Recombinator<G> {
+    type Output;
+
+    fn recombine(&self, genomes: G, rng: &mut ThreadRng) -> Self::Output;
 }
+
+pub struct Recombine<R> {
+    recombinator: R,
+}
+
+impl<R> Recombine<R> {
+    pub const fn new(recombinator: R) -> Self {
+        Self { recombinator }
+    }
+}
+
+impl<R, G> Operator<G> for Recombine<R>
+where
+    R: Recombinator<G>,
+{
+    type Output = R::Output;
+
+    fn apply(&self, genomes: G, rng: &mut ThreadRng) -> Self::Output {
+        self.recombinator.recombine(genomes, rng)
+    }
+}
+impl<R> Composable for Recombine<R> {}
