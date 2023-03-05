b81f193890c77a2f20c672e6e9e491f46d4594dc
Nic McPhee <mcphee@morris.umn.edu>
2023-03-05 11:42:42 -0600

    Add `Map` as a composable operator
    This adds a new `Map` operator that takes a function (as an `Operator`) and applies it to some collection of values, returning a new collection of values.
    
    This also adds a `.map()` method to the `Composable` trait so we can use this to map operators across collections of values in pipelines.

--- src/operator/composable.rs
+++ src/operator/composable.rs
@@ -1,7 +1,10 @@
-use self::{then::Then, and::And};
+// TODO: I had to make this `pub use` to get the imports in `TwoPointXoMutate` to work
+//   and I'm really not sure why. I should do homework on this.
+pub use self::{then::Then, and::And, map::Map};
 
 mod then;
 mod and;
+pub mod map;
 
 // TODO: Rationalize the naming of module files. I think I want
 //   to go back to using `mod.rs` inside sub-folders. At a minimum
@@ -19,6 +22,13 @@ pub trait Composable {
         Self: Sized,
     {
         And::new(self, op)
+    }
+
+    fn map<Op>(self, op: Op) -> Map<Op>
+    where
+        Self: Sized,
+    {
+        Map::new(op)
     }
 
     // fn and_select<S>(self, selector: S) -> Then<Self, Select<S>> {
---
+++ src/operator/composable/map.rs
@@ -0,0 +1,48 @@
+use crate::operator::Operator;
+
+use super::Composable;
+
+pub struct Map<F> {
+    f: F,
+}
+
+impl<F> Map<F> {
+    pub const fn new(f: F) -> Self {
+        Self { f }
+    }
+}
+
+impl<F, Input> Operator<[Input; 2]> for Map<F>
+where
+    F: Operator<Input>,
+{
+    type Output = [F::Output; 2];
+
+    fn apply(&self, input: [Input; 2], rng: &mut rand::rngs::ThreadRng) -> Self::Output {
+        input.map(|x| self.f.apply(x, rng))
+    }
+}
+
+impl<F, Input> Operator<(Input, Input)> for Map<F>
+where
+    F: Operator<Input>,
+{
+    type Output = (F::Output, F::Output);
+
+    fn apply(&self, (x, y): (Input, Input), rng: &mut rand::rngs::ThreadRng) -> Self::Output {
+        (self.f.apply(x, rng), self.f.apply(y, rng))
+    }
+}
+
+impl<F, Input> Operator<Vec<Input>> for Map<F>
+where
+    F: Operator<Input>,
+{
+    type Output = Vec<F::Output>;
+
+    fn apply(&self, input: Vec<Input>, rng: &mut rand::rngs::ThreadRng) -> Self::Output {
+        input.into_iter().map(|x| self.f.apply(x, rng)).collect()
+    }
+}
+
+impl<F> Composable for Map<F> {}
