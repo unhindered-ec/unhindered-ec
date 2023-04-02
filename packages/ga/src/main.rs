01d0921e90a343b41f36da7e98583c37d79a613a
Nic McPhee <mcphee@morris.umn.edu>
2023-04-02 11:26:55 -0500

    Add `ec_lib` as a dependency and bring in scoring and selection
    This sets up the start of `main()` which now has the scoring and selection logic, including dependencies on `ec_lib`.

--- packages/ga/src/main.rs
+++ packages/ga/src/main.rs
@@ -1,3 +1,75 @@
+pub mod args;
+
+use crate::args::{Args, TargetProblem};
+use clap::Parser;
+use ec_lib::{
+    bitstring::{count_ones, hiff, Bitstring},
+    operator::selector::{
+        best::Best, lexicase::Lexicase, tournament::Tournament, weighted::Weighted,
+    }, individual::ec::EcIndividual, test_results::TestResults,
+};
+
 fn main() {
+    let args = Args::parse();
+
+    let scorer = match args.target_problem {
+        TargetProblem::CountOnes => count_ones,
+        TargetProblem::Hiff => hiff,
+    };
+
+    let num_test_cases = match args.target_problem {
+        TargetProblem::CountOnes => args.bit_length,
+        TargetProblem::Hiff => 2 * args.bit_length - 1,
+    };
+
+    let lexicase = Lexicase::new(num_test_cases);
+    let binary_tournament = Tournament::new(2);
+
+    type Pop = Vec<EcIndividual<Bitstring, TestResults<ec_lib::test_results::Error>>>;
+
+    let selector: Weighted<Pop> = Weighted::new(Best, 1)
+        .with_selector(lexicase, 5)
+        .with_selector(binary_tournament, args.population_size - 1);
+
     todo!()
 }
+
+// /// # Errors
+// ///
+// /// This can return an error for a whole host of reasons, mostly because the
+// /// population or the collection of selectors is empty.
+// pub fn do_main(args: Args) -> Result<()> {
+
+//     // Using `Error` in `TestResults<Error>` will have the run favor smaller
+//     // values, where using `Score` (e.g., `TestResults<Score>`) will have the run
+//     // favor larger values.
+//     let population: Vec<EcIndividual<Bitstring, TestResults<Error>>> = new_bitstring_population(
+//         args.population_size,
+//         args.bit_length,
+//         // TODO: I should really have a function somewhere that converts functions
+//         //   that return vectors of scores to `TestResults` structs.
+//         |bitstring| scorer(bitstring).into_iter().map(From::from).sum(),
+//     );
+//     ensure!(population.is_empty().not());
+
+//     let child_maker = TwoPointXoMutate::new(scorer);
+
+//     let mut generation = Generation::new(population, &selector, child_maker);
+
+//     let mut rng = rand::thread_rng();
+
+//     ensure!(generation.population().is_empty().not());
+
+//     (0..args.num_generations).try_for_each(|generation_number| {
+//         generation = match args.run_model {
+//             RunModel::Serial => generation.next()?,
+//             RunModel::Parallel => generation.par_next()?,
+//         };
+//         let best = Best.select(generation.population(), &mut rng)?;
+//         // TODO: Change 2 to be the smallest number of digits needed for
+//         //  args.num_generations-1.
+//         println!("Generation {generation_number:2} best is {best}");
+
+//         Ok(())
+//     })
+// }
