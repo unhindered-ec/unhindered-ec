087fac016973c586055d5d3ceec8c28144e227c3
Nic McPhee <mcphee@morris.umn.edu>
2023-04-17 23:26:31 -0500

    Implement crossover operators for type `Crossover`

--- packages/ec-linear/src/recombinator/two_point_xo.rs
+++ packages/ec-linear/src/recombinator/two_point_xo.rs
@@ -3,20 +3,12 @@ use rand::{rngs::ThreadRng, Rng};
 
 use ec_core::operator::recombinator::Recombinator;
 
+use super::crossover::Crossover;
+
 pub struct TwoPointXo;
 
-// TODO: Note that `TwoPointXo` doesn't strictly need
-//   the two vectors to have the same length, but the
-//   swapped regions need to "make sense" for both parent
-//   genomes. So we could just make sure that both
-//   `first` and `second` are < the minimum length
-//   and we'd be OK. I'm not sure that's really how we'd
-//   want to implement `TwoPointXo` for differing lengths,
-//   though. I suspect it would make more sense to ensure
-//   that the length of the swapped region is less than the
-//   the length of the shorter genome, but not require that
-//   they line up. That's really sounding like a different
-//   operator than this one, though.
+// TODO: Remove the `Vec<T>` versions when we're done migrating
+//   to the struct-based version of `Bitstring`.
 impl<T> Recombinator<[Vec<T>; 2]> for TwoPointXo {
     type Output = Vec<T>;
 
@@ -50,6 +42,58 @@ impl<T> Recombinator<(Vec<T>, Vec<T>)> for TwoPointXo {
     fn recombine(
         &self,
         (first_genome, second_genome): (Vec<T>, Vec<T>),
+        rng: &mut ThreadRng,
+    ) -> Result<Self::Output> {
+        self.recombine([first_genome, second_genome], rng)
+    }
+}
+
+// TODO: Note that `TwoPointXo` doesn't strictly need
+//   the two vectors to have the same length, but the
+//   swapped regions need to "make sense" for both parent
+//   genomes. So we could just make sure that both
+//   `first` and `second` are < the minimum length
+//   and we'd be OK. I'm not sure that's really how we'd
+//   want to implement `TwoPointXo` for differing lengths,
+//   though. I suspect it would make more sense to ensure
+//   that the length of the swapped region is less than the
+//   the length of the shorter genome, but not require that
+//   they line up. That's really sounding like a different
+//   operator than this one, though.
+impl<G> Recombinator<[G; 2]> for TwoPointXo
+where
+    G: Crossover
+{
+    type Output = G;
+
+    fn recombine(&self, [mut first_genome, mut second_genome]: [G; 2], rng: &mut ThreadRng) -> Result<Self::Output> {
+        ensure!(
+            first_genome.size() == second_genome.size(),
+            "Attempted to perform TwoPointXo on genomes of different lengths {} and {}",
+            first_genome.size(),
+            second_genome.size()
+        );
+        let len = first_genome.size();
+        let mut first = rng.gen_range(0..len);
+        let mut second = rng.gen_range(0..len);
+        if second < first {
+            (first, second) = (second, first);
+        }
+        first_genome.crossover_segment(&mut second_genome, first..second)?;
+
+        Ok(first_genome)
+    }
+}
+
+impl<G> Recombinator<(G, G)> for TwoPointXo
+where
+    G: Crossover
+{
+    type Output = G;
+
+    fn recombine(
+        &self,
+        (first_genome, second_genome): (G, G),
         rng: &mut ThreadRng,
     ) -> Result<Self::Output> {
         self.recombine([first_genome, second_genome], rng)
--- packages/ec-linear/src/recombinator/uniform_xo.rs
+++ packages/ec-linear/src/recombinator/uniform_xo.rs
@@ -3,8 +3,14 @@ use rand::{rngs::ThreadRng, Rng};
 
 use ec_core::operator::recombinator::Recombinator;
 
+use crate::genome::LinearGenome;
+
+use super::crossover::Crossover;
+
 pub struct UniformXo;
 
+// TODO: We should get rid of the `Vec<T>` versions when 
+//   we've completed the migration to a struct-based `Bitstring`.
 impl<T: Clone> Recombinator<[Vec<T>; 2]> for UniformXo {
     type Output = Vec<T>;
 
@@ -43,3 +49,47 @@ impl<T: Clone> Recombinator<(Vec<T>, Vec<T>)> for UniformXo {
         self.recombine([first_genome, second_genome], rng)
     }
 }
+
+impl<G> Recombinator<[G; 2]> for UniformXo
+where
+    G: Crossover,
+{
+    type Output = G;
+
+    fn recombine(
+        &self,
+        [mut first_genome, mut second_genome]: [G; 2],
+        rng: &mut ThreadRng,
+    ) -> Result<Self::Output> {
+        ensure!(
+            first_genome.size() == second_genome.size(),
+            "Attempted to perform UniformXo on genomes of different length: {} and {}",
+            first_genome.size(),
+            second_genome.size()
+        );
+        let len = first_genome.size();
+        for i in 0..len {
+            if rng.gen_bool(0.5) {
+                first_genome.crossover_gene(&mut second_genome, i)?;
+            }
+        }
+
+        Ok(first_genome)
+    }
+}
+
+impl<G> Recombinator<(G, G)> for UniformXo 
+where
+    G: Crossover
+{
+    type Output = G;
+
+    fn recombine(
+        &self,
+        (first_genome, second_genome): (G, G),
+        rng: &mut ThreadRng,
+    ) -> Result<Self::Output> {
+        self.recombine([first_genome, second_genome], rng)
+    }
+}
+
