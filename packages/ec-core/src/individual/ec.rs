7370d706bb5187352ec9f6100d4c1bdeca250e69
Nic McPhee <mcphee@morris.umn.edu>
2023-04-25 14:40:09 -0500

    Implement `Generator` for `EcIndividual`
    This implements the `Generator` trait for `EcIndividual`, using the new `Scorer` trait.
    
    I'm guessing that the `context.scorer.score(&genome)` call is an interesting example of a zero-cost abstraction. While it looks like there's a lot happening there, my guess is that the compiler turns that into a simple function call when the scorer is a simple function like `count_ones`.

--- packages/ec-core/src/individual/ec.rs
+++ packages/ec-core/src/individual/ec.rs
@@ -6,7 +6,9 @@ use std::{
 
 use rand::rngs::ThreadRng;
 
-use super::{Generate, Individual};
+use crate::generator::Generator;
+
+use super::{scorer::Scorer, Generate, Individual};
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 #[allow(clippy::module_name_repetitions)]
@@ -34,6 +36,43 @@ impl<G, R> EcIndividual<G, R> {
             genome,
             test_results,
         }
+    }
+}
+
+impl<G: Eq, R: Ord> Ord for EcIndividual<G, R> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.test_results.cmp(&other.test_results)
+    }
+}
+
+impl<G: PartialEq, R: PartialOrd> PartialOrd for EcIndividual<G, R> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.test_results.partial_cmp(&other.test_results)
+    }
+}
+
+// TODO: Maybe change R to implement `Display` and have `TestResults` have a
+//   nice-ish display function.
+impl<G: Display, R: Debug> Display for EcIndividual<G, R> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]\n{:?}", self.genome(), self.test_results())
+    }
+}
+
+pub struct GeneratorContext<GC, S> {
+    pub genome_context: GC,
+    pub scorer: S,
+}
+
+impl<G, GC, S, R> Generator<EcIndividual<G, R>, GeneratorContext<GC, S>> for ThreadRng
+where
+    Self: Generator<G, GC>,
+    S: Scorer<G, R>,
+{
+    fn generate(&mut self, context: &GeneratorContext<GC, S>) -> EcIndividual<G, R> {
+        let genome = self.generate(&context.genome_context);
+        let test_results = context.scorer.score(&genome);
+        EcIndividual::new(genome, test_results)
     }
 }
 
@@ -85,25 +124,5 @@ impl<G, R> Generate for EcIndividual<G, R> {
             genome,
             test_results,
         }
-    }
-}
-
-impl<G: Eq, R: Ord> Ord for EcIndividual<G, R> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.test_results.cmp(&other.test_results)
-    }
-}
-
-impl<G: PartialEq, R: PartialOrd> PartialOrd for EcIndividual<G, R> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.test_results.partial_cmp(&other.test_results)
-    }
-}
-
-// TODO: Maybe change R to implement `Display` and have `TestResults` have a
-//   nice-ish display function.
-impl<G: Display, R: Debug> Display for EcIndividual<G, R> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{}]\n{:?}", self.genome(), self.test_results())
     }
 }
