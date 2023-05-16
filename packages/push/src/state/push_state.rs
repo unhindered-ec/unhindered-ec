ac2e1feef8949f0ee9ca5e0f347ba96a1fbcc2ba
Nic McPhee <mcphee@morris.umn.edu>
2023-05-16 10:29:06 -0500

    Group the instructions by type
    This creates new `IntInstruction` and `BoolInstruction` enums that group together all the instructions for a given type (int or bool) into a single `enum`.

--- packages/push/src/state/push_state.rs
+++ packages/push/src/state/push_state.rs
@@ -47,12 +47,24 @@ impl State for PushState {
     }
 }
 
+#[derive(Debug)]
 pub enum PushInstruction {
-    Int(i128),
-    Bool(bool),
-    IntAdd,
-    IntIsEven,
+    BoolInstruction(BoolInstruction),
+    IntInstruction(IntInstruction),
+}
+
+#[derive(Debug)]
+pub enum BoolInstruction {
+    Push(bool),
     BoolOr,
+    BoolAnd,
+}
+
+#[derive(Debug)]
+pub enum IntInstruction {
+    Push(i128),
+    Add,
+    IsEven,
 }
 
 #[inline(always)]
@@ -66,29 +78,112 @@ fn pop2<T>(stack: &mut Vec<T>) -> Option<(T, T)> {
     }
 }
 
+impl PushInstruction {
+    pub fn push_bool(b: bool) -> Self {
+        BoolInstruction::Push(b).into()
+    }
+
+    pub fn push_int(i: i128) -> Self {
+        IntInstruction::Push(i).into()
+    }
+}
+
 impl Instruction<PushState> for PushInstruction {
     fn perform(&self, state: &mut PushState) {
         match self {
-            PushInstruction::Int(i) => state.int.push(*i),
-            PushInstruction::Bool(b) => state.bool.push(*b),
-            PushInstruction::IntAdd => {
+            PushInstruction::BoolInstruction(i) => i.perform(state),
+            PushInstruction::IntInstruction(i) => i.perform(state),
+        }
+    }
+}
+
+impl Instruction<PushState> for BoolInstruction {
+    fn perform(&self, state: &mut PushState) {
+        match self {
+            BoolInstruction::Push(b) => state.bool.push(*b),
+            BoolInstruction::BoolAnd => {
+                if let Some((x, y)) = pop2(&mut state.bool) {
+                    state.bool.push(x && y);
+                }
+            }
+            BoolInstruction::BoolOr => {
+                if let Some((x, y)) = pop2(&mut state.bool) {
+                    state.bool.push(x || y);
+                }
+            },
+        }
+    }
+}
+
+impl From<BoolInstruction> for PushInstruction {
+    fn from(instr: BoolInstruction) -> Self {
+        Self::BoolInstruction(instr)
+    }
+}
+
+impl Instruction<PushState> for IntInstruction {
+    fn perform(&self, state: &mut PushState) {
+        match self {
+            IntInstruction::Push(i) => state.int.push(*i),
+            IntInstruction::Add => {
+                // TODO: We should probably check that this addition succeeds and do something
+                //   sensible if it doesn't. That requires having these return a `Result` or
+                //   `Option`, however, which we don't yet do.
                 if let Some((x, y)) = pop2(&mut state.int) {
-                    // TODO: We should probably check that this addition succeeds and do something
-                    //   sensible if it doesn't. That requires having these return a `Result` or
-                    //   `Option`, however, which we don't yet do.
                     state.int.push(x + y);
                 }
             }
-            PushInstruction::IntIsEven => {
+            IntInstruction::IsEven => {
                 if let Some(i) = state.int.pop() {
                     state.bool.push(i % 2 == 0);
                 }
             }
-            PushInstruction::BoolOr => {
-                if let Some((x, y)) = pop2(&mut state.bool) {
-                    state.bool.push(x || y);
-                }
-            }
         }
+    }
+}
+
+impl From<IntInstruction> for PushInstruction {
+    fn from(instr: IntInstruction) -> Self {
+        Self::IntInstruction(instr)
+    }
+}
+
+#[cfg(test)]
+mod simple_check {
+    use crate::state::push_state::{PushInstruction, PushState};
+
+    use super::*;
+
+    #[test]
+    fn run_simple_program() {
+        fn push_bool(b: bool) -> PushInstruction {
+            PushInstruction::push_bool(b)
+        }
+
+        fn push_int(i: i128) -> PushInstruction {
+            PushInstruction::push_int(i)
+        }
+
+        // TODO: Can I make this a Vec<dyn Into<PushInstruction>> and
+        //   then just `map.(Into::into)` across them all so I don't
+        //   have to repeat the `.into()` over and over?        
+        let program = vec![
+            push_int(5),
+            push_int(8),
+            push_bool(true),
+            push_int(9),
+            BoolInstruction::BoolOr.into(),
+            IntInstruction::Add.into(),
+            push_int(6),
+            IntInstruction::IsEven.into(),
+            BoolInstruction::BoolAnd.into(),
+        ];
+        let mut state = PushState::new(program);
+        println!("{state:?}");
+        state.run_to_completion();
+        println!("{state:?}");
+        assert!(state.exec().is_empty());
+        assert_eq!(state.int(), &vec![5, 17]);
+        assert_eq!(state.bool(), &vec![true]);
     }
 }
