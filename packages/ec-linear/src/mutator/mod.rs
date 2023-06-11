83d1527995f90cbd5cd67616e7f23780e73fa7f0
Nic McPhee <mcphee@morris.umn.edu>
2023-06-11 12:43:28 -0500

    Add implementation of UMAD mutation operator
    This gets what I think is most of `Umad` done and working. `main.rs` still doesn't compile, though, so there's _something_ missing, probably either here or in `Plushy`.
    
    I should also at least write a "smoke test" for `Umad` to make sure the types are all working there.

--- packages/ec-linear/src/mutator/mod.rs
+++ packages/ec-linear/src/mutator/mod.rs
@@ -1,2 +1,3 @@
+pub mod umad;
 pub mod with_one_over_length;
 pub mod with_rate;
---
+++ packages/ec-linear/src/mutator/umad.rs
@@ -0,0 +1,71 @@
+use ec_core::{generator::Generator, genome::Genome, operator::mutator::Mutator};
+use rand::{rngs::ThreadRng, Rng};
+
+use crate::genome::LinearGenome;
+
+/// UMAD = Uniform Mutation through random Addition and Deletion
+pub struct Umad<GeneContext> {
+    addition_rate: f64,
+    deletion_rate: f64,
+    // Provides the context needed to generate a new, random gene
+    // during the addition phase.
+    gene_context: GeneContext,
+}
+
+impl<GeneContext> Umad<GeneContext> {
+    pub const fn new(addition_rate: f64, deletion_rate: f64, gene_context: GeneContext) -> Self {
+        Self {
+            addition_rate,
+            deletion_rate,
+            gene_context,
+        }
+    }
+
+    fn new_gene<G>(&self, rng: &mut ThreadRng) -> G::Gene
+    where
+        G: Genome,
+        ThreadRng: Generator<G::Gene, GeneContext>,
+    {
+        rng.generate(&self.gene_context)
+    }
+}
+
+impl<G, GeneContext> Mutator<G> for Umad<GeneContext>
+where
+    G: LinearGenome + IntoIterator<Item = G::Gene> + FromIterator<G::Gene>,
+    ThreadRng: Generator<G::Gene, GeneContext>,
+{
+    fn mutate(&self, genome: G, rng: &mut ThreadRng) -> anyhow::Result<G> {
+        // Addition pass
+        Ok(genome
+            .into_iter()
+            .flat_map(|gene| {
+                let add_gene = rng.gen_bool(self.addition_rate);
+                let delete_gene = rng.gen_bool(self.deletion_rate);
+                match (add_gene, delete_gene) {
+                    (true, _) => {
+                        // Add a new gene, and decide whether to then immediately delete it.
+                        let delete_new_gene = rng.gen_bool(self.deletion_rate);
+                        match (delete_gene, delete_new_gene) {
+                            // Delete both the given gene and the new gene.
+                            (true, true) => [None, None],
+                            // Delete the given gene but keep the new gene.
+                            (true, false) => [None, Some(self.new_gene::<G>(rng))],
+                            // Keep the given gene but delete the new gene.
+                            (false, true) => [Some(gene), None],
+                            // Keep both the given gene and the new gene.
+                            (false, false) => [Some(gene), Some(self.new_gene::<G>(rng))],
+                        }
+                    }
+                    // Do not add a gene, and delete the given gene.
+                    (false, true) => [None, None],
+                    // Do not add a gene, and leave the given gene alone.
+                    (false, false) => [Some(gene), None],
+                }
+            })
+            .flatten()
+            .collect::<G>())
+    }
+}
+
+// TODO: Write a simple test for `Umad` on, say, `Vec<i32>`.
