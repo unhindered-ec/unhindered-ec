2f2879884a4d467e660bab127fc312320a0c756b
Nic McPhee <mcphee@morris.umn.edu>
2023-06-11 13:05:14 -0500

    Add MizardX's nice improvement to logic in `Umad::mutate()`
    MizardX@Twitch posted a much nicer version of this logic on Discord, and I wanted to capture that.

--- packages/ec-linear/src/mutator/umad.rs
+++ packages/ec-linear/src/mutator/umad.rs
@@ -40,28 +40,45 @@ where
         Ok(genome
             .into_iter()
             .flat_map(|gene| {
+                // The body of this closure is due to MizardX@Twitch; much nicer than my original approach.
                 let add_gene = rng.gen_bool(self.addition_rate);
                 let delete_gene = rng.gen_bool(self.deletion_rate);
-                match (add_gene, delete_gene) {
-                    (true, _) => {
-                        // Add a new gene, and decide whether to then immediately delete it.
-                        let delete_new_gene = rng.gen_bool(self.deletion_rate);
-                        match (delete_gene, delete_new_gene) {
-                            // Delete both the given gene and the new gene.
-                            (true, true) => [None, None],
-                            // Delete the given gene but keep the new gene.
-                            (true, false) => [None, Some(self.new_gene::<G>(rng))],
-                            // Keep the given gene but delete the new gene.
-                            (false, true) => [Some(gene), None],
-                            // Keep both the given gene and the new gene.
-                            (false, false) => [Some(gene), Some(self.new_gene::<G>(rng))],
-                        }
-                    }
-                    // Do not add a gene, and delete the given gene.
-                    (false, true) => [None, None],
-                    // Do not add a gene, and leave the given gene alone.
-                    (false, false) => [Some(gene), None],
-                }
+                // only called when `add_gene` is true
+                let delete_new_gene = add_gene && rng.gen_bool(self.deletion_rate);
+
+                #[allow(clippy::match_bool)]
+                let old_gene = match delete_gene {
+                    false => Some(gene),
+                    true => None,
+                };
+
+                let new_gene = match (add_gene, delete_new_gene) {
+                    (true, false) => Some(self.new_gene::<G>(rng)),
+                    _ => None,
+                };
+
+                [old_gene, new_gene]
+
+                // match (add_gene, delete_gene) {
+                //     (true, _) => {
+                //         // Add a new gene, and decide whether to then immediately delete it.
+                //         let delete_new_gene = rng.gen_bool(self.deletion_rate);
+                //         match (delete_gene, delete_new_gene) {
+                //             // Delete both the given gene and the new gene.
+                //             (true, true) => [None, None],
+                //             // Delete the given gene but keep the new gene.
+                //             (true, false) => [None, Some(self.new_gene::<G>(rng))],
+                //             // Keep the given gene but delete the new gene.
+                //             (false, true) => [Some(gene), None],
+                //             // Keep both the given gene and the new gene.
+                //             (false, false) => [Some(gene), Some(self.new_gene::<G>(rng))],
+                //         }
+                //     }
+                //     // Do not add a gene, and delete the given gene.
+                //     (false, true) => [None, None],
+                //     // Do not add a gene, and leave the given gene alone.
+                //     (false, false) => [Some(gene), None],
+                // }
             })
             .flatten()
             .collect::<G>())
