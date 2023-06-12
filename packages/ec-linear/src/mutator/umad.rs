1962230a8a8c776dfa8e7a603c7f97c895ee13b4
Nic McPhee <mcphee@morris.umn.edu>
2023-06-12 18:51:31 -0500

    Write a simple smoke test for `Umad`
    This provides a simple smoke test for `Umad` that makes sure that we can run `Umad` on a `Vector<char>` linear genome. We check that it actually inserts a few new characters, deletes a few characters, and that the remaining characters are a subsequence of the original string.
    
    This is a stochastic test, so it will fail occasionally, but not too often.

--- packages/ec-linear/src/mutator/umad.rs
+++ packages/ec-linear/src/mutator/umad.rs
@@ -86,3 +86,68 @@ where
 }
 
 // TODO: Write a simple test for `Umad` on, say, `Vec<i32>`.
+
+#[cfg(test)]
+mod test {
+    use rand::thread_rng;
+
+    use crate::genome::vector::Vector;
+
+    use super::*;
+
+    fn count_missing(short: &Vec<char>, long: &Vec<char>) -> Option<usize> {
+        let mut short_index = 0;
+        let mut long_index = 0;
+        let mut num_missing = 0;
+        while short_index < short.len() && long_index < long.len() {
+            if short[short_index] == long[long_index] {
+                short_index += 1;
+            } else {
+                num_missing += 1;
+            }
+            long_index += 1;
+        }
+        (short_index == short.len()).then_some(num_missing + (long.len() - long_index))
+    }
+
+    #[test]
+    #[allow(clippy::unwrap_used)]
+    // This is stochastic, and will fail sometimes.
+    fn smoke_test() {
+        let mut rng = thread_rng();
+
+        let char_options: [char; 1] = ['x'];
+        let umad = Umad::new(0.3, 0.3, char_options);
+
+        let parent_chars = "Morris, Minnesota".chars().collect::<Vec<_>>();
+        let parent = Vector {
+            genes: parent_chars.clone(),
+        };
+
+        let child = umad.mutate(parent, &mut rng).unwrap();
+
+        // println!("{child:?}");
+        let num_xs = child.genes.iter().filter(|c| **c == 'x').count();
+        assert!(
+            num_xs > 0,
+            "Expected at least one 'x' to be added, but none were."
+        );
+        let remaining_chars = child
+            .genes
+            .iter()
+            .filter(|c| **c != 'x')
+            .copied()
+            .collect::<Vec<_>>();
+        // println!("There were {num_xs} 'x's.");
+        // println!("The remaining characters were {remaining_chars:?}");
+        let num_missing = count_missing(&remaining_chars, &parent_chars);
+        assert!(
+            num_missing.is_some(),
+            "The remaining chars {remaining_chars:?} should have been an ordered subsequence of the parent chars {parent_chars:?}."
+        );
+        assert!(
+            num_missing.unwrap() > 0,
+            "There should have been at least one character dropped from the parent in {child:?}"
+        );
+    }
+}
