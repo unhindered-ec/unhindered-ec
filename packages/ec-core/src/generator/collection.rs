8216469298d64392e2f1809d7aa16e7541a978a7
Nic McPhee <mcphee@morris.umn.edu>
2023-06-20 11:50:46 -0500

    Add doc comments to `CollectionGenerator`

--- packages/ec-core/src/generator/collection.rs
+++ packages/ec-core/src/generator/collection.rs
@@ -4,12 +4,26 @@ use rand::rngs::ThreadRng;
 
 use super::Generator;
 
+/// Information for generating a collection of random elements.
+///
+/// `size` indicates how many elements to generate.
+/// `element_generator` is used to generate individual elements.
 #[allow(clippy::module_name_repetitions)]
 pub struct CollectionGenerator<C> {
     pub size: usize,
     pub element_generator: C,
 }
 
+/// Generate a `Vec` of random elements.
+///
+/// The number of element and the mechanism for generating
+/// random elements are specified in the `CollectionGenerator`
+/// struct.
+///
+/// # Errors
+///
+/// This returns an `anyhow::Error` generating any of
+/// the elements returns an error.
 impl<T, C> Generator<Vec<T>> for CollectionGenerator<C>
 where
     C: Generator<T>,
