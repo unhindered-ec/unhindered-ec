24774109b46c647168a86b4ca1c1be3a6c10135a
Nic McPhee <mcphee@morris.umn.edu>
2023-07-17 12:27:32 -0500

    Add all the `int` and `bool` instructions that are in Clojush
    This fleshes out the `int` and `bool` instruction sets. I used the Clojush instruction sets as the model here, and added everything that made sense from those. There are a few that I only mentioned in comments because they needed a stack we didn't have (`BoolFromFloat`), or I wasn't sure we actually wanted them (`BooleanInvertFirstThenAnd`).
    
    If we're going to use Rust, we have potential overflow issues on several of the int operations. I'm not sure how we want to handle that. At the moment I just skip the action and push the results back on, but I doubt that's actually a great solution.
    
    It probably makes more sense to have instructions return an `Option` or a `Result`, and then have the state system only update the stacks if we get a `Some()` or `Ok()` result?
    
    There's also a ton of duplicate code, here, and I'm sure we can do better. Not sure if that's a matter of extracting functions, or if we should be using macros for that.

--- packages/push/src/instruction/bool.rs
+++ packages/push/src/instruction/bool.rs
@@ -1,18 +1,32 @@
+use strum_macros::EnumIter;
+
 use super::{Instruction, PushInstruction};
 use crate::{push_vm::push_state::PushState, util::pop2};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, EnumIter)]
 #[allow(clippy::module_name_repetitions)]
 pub enum BoolInstruction {
     Push(bool),
+    BoolNot,
     BoolOr,
     BoolAnd,
+    BoolXor,
+    // Do we really want either of these? Do they get used?
+    // BooleanInvertFirstThenAnd,
+    // BoolInvertSecondThenAnd,
+    BoolFromInt,
+    // BoolFromFloat,
 }
 
 impl Instruction<PushState> for BoolInstruction {
     fn perform(&self, state: &mut PushState) {
         match self {
             Self::Push(b) => state.bool.push(*b),
+            Self::BoolNot => {
+                if let Some(x) = state.bool.pop() {
+                    state.bool.push(!x);
+                }
+            }
             Self::BoolAnd => {
                 if let Some((x, y)) = pop2(&mut state.bool) {
                     state.bool.push(x && y);
@@ -21,6 +35,16 @@ impl Instruction<PushState> for BoolInstruction {
             Self::BoolOr => {
                 if let Some((x, y)) = pop2(&mut state.bool) {
                     state.bool.push(x || y);
+                }
+            }
+            Self::BoolXor => {
+                if let Some((x, y)) = pop2(&mut state.bool) {
+                    state.bool.push(x != y);
+                }
+            }
+            Self::BoolFromInt => {
+                if let Some(x) = state.int.pop() {
+                    state.bool.push(x != 0);
                 }
             }
         }
--- packages/push/src/instruction/int.rs
+++ packages/push/src/instruction/int.rs
@@ -1,27 +1,95 @@
+use strum_macros::EnumIter;
+
 use super::{Instruction, PushInstruction};
 use crate::{push_vm::push_state::PushState, util::pop2};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, EnumIter)]
 #[allow(clippy::module_name_repetitions)]
 pub enum IntInstruction {
     Push(i64),
+    Negate,
+    Abs,
+    Inc,
+    Dec,
     Add,
     Subtract,
     Multiply,
     ProtectedDivide,
+    Mod,
+    Power,
+    Square,
+    // We can't easily convert from i64 to f64, etc.,
+    // so we might need to do the log(n) integer
+    // implementation of sqrt.
+    // Sqrt,
     IsEven,
+    IsOdd,
+    LessThan,
+    LessThanEqual,
+    GreaterThan,
+    GreaterThanEqual,
+    FromBoolean,
+    Min,
+    Max,
 }
 
 impl Instruction<PushState> for IntInstruction {
+    #[allow(clippy::too_many_lines, clippy::cognitive_complexity)]
     fn perform(&self, state: &mut PushState) {
         match self {
             Self::Push(i) => state.int.push(*i),
+            Self::Negate => {
+                if let Some(i) = state.int.pop() {
+                    state.int.push(-i);
+                }
+            }
+            Self::Abs => {
+                if let Some(i) = state.int.pop() {
+                    state.int.push(i.abs());
+                }
+            }
+            Self::Inc => {
+                if let Some(x) = state.int.pop() {
+                    if let Some(result) = x.checked_add(1) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(x);
+                    }
+                }
+            }
+            Self::Dec => {
+                if let Some(x) = state.int.pop() {
+                    if let Some(result) = x.checked_sub(1) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(x);
+                    }
+                }
+            }
+            Self::Square => {
+                if let Some(x) = state.int.pop() {
+                    if let Some(result) = x.checked_mul(x) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(x);
+                    }
+                }
+            }
             Self::Add => {
                 // TODO: We should probably check that this addition succeeds and do something
                 //   sensible if it doesn't. That requires having these return a `Result` or
                 //   `Option`, however, which we don't yet do.
                 if let Some((x, y)) = pop2(&mut state.int) {
-                    state.int.push(x + y);
+                    if let Some(result) = x.checked_add(y) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(y);
+                        state.int.push(x);
+                    }
                 }
             }
             Self::Subtract => {
@@ -29,7 +97,13 @@ impl Instruction<PushState> for IntInstruction {
                 //   sensible if it doesn't. That requires having these return a `Result` or
                 //   `Option`, however, which we don't yet do.
                 if let Some((x, y)) = pop2(&mut state.int) {
-                    state.int.push(x - y);
+                    if let Some(result) = x.checked_sub(y) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(y);
+                        state.int.push(x);
+                    }
                 }
             }
             Self::Multiply => {
@@ -37,7 +111,13 @@ impl Instruction<PushState> for IntInstruction {
                 //   sensible if it doesn't. That requires having these return a `Result` or
                 //   `Option`, however, which we don't yet do.
                 if let Some((x, y)) = pop2(&mut state.int) {
-                    state.int.push(x * y);
+                    if let Some(result) = x.checked_mul(y) {
+                        state.int.push(result);
+                    } else {
+                        // Do nothing, i.e., put the arguments back
+                        state.int.push(y);
+                        state.int.push(x);
+                    }
                 }
             }
             Self::ProtectedDivide => {
@@ -52,9 +132,79 @@ impl Instruction<PushState> for IntInstruction {
                     }
                 }
             }
+            Self::Mod => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    if y == 0 {
+                        // Do nothing, i.e., put the values back
+                        state.int.push(y);
+                        state.int.push(x);
+                    } else {
+                        state.int.push(x % y);
+                    }
+                }
+            }
+            // TODO: I'm not convinced that Clojush handles negative y correctly.
+            // TODO: I assume that this blows up for large values of y and I'm not sure
+            //   what actually happens. There's a `checked_pow` function that might be
+            //   the preferable choice?
+            Self::Power => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    if let Ok(y) = u32::try_from(y) {
+                        state.int.push(x.pow(y));
+                    } else {
+                        // Do nothing, i.e., put the values back
+                        state.int.push(y);
+                        state.int.push(x);
+                    }
+                }
+            }
             Self::IsEven => {
                 if let Some(i) = state.int.pop() {
                     state.bool.push(i % 2 == 0);
+                }
+            }
+            Self::IsOdd => {
+                if let Some(i) = state.int.pop() {
+                    state.bool.push(i % 2 != 0);
+                }
+            }
+            Self::LessThan => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.bool.push(x < y);
+                }
+            }
+            Self::LessThanEqual => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.bool.push(x <= y);
+                }
+            }
+            Self::GreaterThan => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.bool.push(x > y);
+                }
+            }
+            Self::GreaterThanEqual => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.bool.push(x >= y);
+                }
+            }
+            Self::FromBoolean => {
+                if let Some(b) = state.bool.pop() {
+                    if b {
+                        state.int.push(1);
+                    } else {
+                        state.int.push(0);
+                    }
+                }
+            }
+            Self::Min => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.int.push(x.min(y));
+                }
+            }
+            Self::Max => {
+                if let Some((x, y)) = pop2(&mut state.int) {
+                    state.int.push(x.max(y));
                 }
             }
         }
