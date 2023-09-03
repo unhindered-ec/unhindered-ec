3078edf4eb1dc0dcd8ad1b6e0564edd01f70499d
Nic McPhee <mcphee@morris.umn.edu>
2023-09-03 17:31:35 -0500

    Address casting warnings in `hiff()`
    Updates to Clippy were complaining about `len as i64`, which was casting a `usize` to a `i64`. This had bugged me at the time, but there weren't any warnings so I let it go, which illustrates the importance of warnings like this.
    
    So now this fixes that. I assert in `hiff()` that the length will fit in an `i64`, and then `do_hiff()` can safely convert that length and all sub-lengths to `i64` using `as i64`.

--- packages/ec-linear/src/genome/demo_scorers.rs
+++ packages/ec-linear/src/genome/demo_scorers.rs
@@ -5,8 +5,25 @@ pub fn count_ones(bits: &[bool]) -> TestResults<test_results::Score> {
     bits.iter().map(|bit| i64::from(*bit)).collect()
 }
 
+/// # Panics
+///
+/// This will panic if the length of the bitstring won't fit in an
+/// `i64`. Since we're (currently) using an `i64` to store the score,
+/// and since the largest possible score is the length of the bitstring,
+/// it's necessary that the length fit in an `i64`. If it doesn't we'll
+/// panic here.
 #[must_use]
 pub fn hiff(bits: &[bool]) -> TestResults<test_results::Score> {
+    // Since the scores are represented as `i64`, and the largest
+    // possible score is the length of `bits`, we need to know that
+    // `bits.len()` can fit in an `i64`. If this is true we can use
+    // `as` in `do_hiff` to freely convert between the length (`usize`)
+    // and `i64`.
+    assert!(
+        i64::try_from(bits.len()).is_ok(),
+        "The length of the bitstring {} won't fit in an `i64`.",
+        bits.len()
+    );
     let num_scores = 2 * bits.len() - 1;
     let mut scores = Vec::with_capacity(num_scores);
     do_hiff(bits, &mut scores);
@@ -15,15 +32,18 @@ pub fn hiff(bits: &[bool]) -> TestResults<test_results::Score> {
 
 fn do_hiff(bits: &[bool], scores: &mut Vec<i64>) -> bool {
     let len = bits.len();
+    // This should be safe because of the `assert!` above in `hiff()`.
+    #[allow(clippy::cast_possible_wrap)]
+    let len_as_score = len as i64;
     if len < 2 {
-        scores.push(len as i64);
+        scores.push(len_as_score);
         true
     } else {
         let half_len = len / 2;
         let left_all_same = do_hiff(&bits[..half_len], scores);
         let right_all_same = do_hiff(&bits[half_len..], scores);
         if left_all_same && right_all_same && bits[0] == bits[half_len] {
-            scores.push(bits.len() as i64);
+            scores.push(len_as_score);
             true
         } else {
             scores.push(0);
