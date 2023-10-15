df11c5d50f1124228999004c6be75e625b4e5ace
Justus Flügel <justusfluegel@gmail.com>
2023-10-15 23:19:31 +0200

    fix(push): Update transactional/mod.rs to use the wrapper_transaction! macro
    Signed-off-by: Justus Flügel <justusfluegel@gmail.com>

--- packages/push/src/push_vm/stack/transactional/mod.rs
+++ packages/push/src/push_vm/stack/transactional/mod.rs
@@ -1,2 +1,43 @@
-pub mod additive;
-pub mod substractive;
+use crate::{error::into_state::StateMut, push_vm::PushInteger, type_eq::TypeEq};
+
+use super::traits::has_stack::{HasStack, HasStackMut};
+
+#[cfg(feature = "macros")]
+use push_macros::wrapper_transaction;
+
+fn test<T>() {}
+
+fn foo<S>(state: S)
+where
+    S: HasStackMut<bool> + StateMut,
+{
+    let transaction = {
+        struct Test {}
+
+        impl Into<usize> for Test {
+            fn into(self) -> usize {
+                0
+            }
+        }
+
+        Test {}
+    };
+
+    let val: usize = transaction.into();
+
+    let transaction = wrapper_transaction!(state, stacks = {
+        bool,
+        mut PushInteger: Cloning
+    });
+    // let transaction = state.new_transaction::<for_stacks! { bool: Cloning }>()
+}
+
+trait Transaction<'a, Stack> {
+    fn create(stack: &'a mut Stack) -> Self;
+}
+
+trait CreateTransaction<Backend, Transaction> {
+    fn new_transaction<U: TypeEq<This = Backend>>(&mut self) -> Transaction;
+}
+
+struct SomeValue<const N: usize, T>(T);
