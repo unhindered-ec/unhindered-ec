283a902a7c38477e833625d98de250ad454f5f09
Nic McPhee <mcphee@morris.umn.edu>
2023-11-27 13:45:03 -0600

    Implement basic floating point Push instructions
    This uses `OrderedFloat` to wrap all the floating point values so that things like `FloatInstruction` can implement `Eq` and `PartialEq`.
    
    The implementation of the four basic arithmetic instructions are essentially identical to those for integers. The only real difference is that here we don't need to check for integer arithmetic errors (like overflow).
    
    I suspect there's a generic version of instruction for numbers that can handle both integers and floats, although I haven't thought about what we've have to do with those errors.

--- packages/push/src/instruction/float.rs
+++ packages/push/src/instruction/float.rs
@@ -1,12 +1,17 @@
-use crate::{instruction::MapInstructionError, push_vm::HasStack};
-
 use super::{Instruction, PushInstruction, PushInstructionError};
+use crate::{
+    error::Error,
+    error::InstructionResult,
+    instruction::MapInstructionError,
+    push_vm::{stack::StackPush, HasStack},
+};
+use ordered_float::OrderedFloat;
 use strum_macros::EnumIter;
 
 #[derive(Debug, strum_macros::Display, Copy, Clone, EnumIter, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum FloatInstruction {
-    Push(f64),
+    Push(OrderedFloat<f64>),
     Add,
     Subtract,
     Multiply,
@@ -21,14 +26,38 @@ impl From<FloatInstruction> for PushInstruction {
 
 impl<S> Instruction<S> for FloatInstruction
 where
-    S: Clone + HasStack<f64>,
+    S: Clone + HasStack<OrderedFloat<f64>>,
 {
     type Error = PushInstructionError;
 
-    fn perform(&self, state: S) -> crate::error::InstructionResult<S, Self::Error> {
+    fn perform(&self, mut state: S) -> InstructionResult<S, Self::Error> {
+        let float_stack = state.stack_mut::<OrderedFloat<f64>>();
         match self {
             Self::Push(f) => state.with_push(*f).map_err_into(),
-            _ => todo!(),
+
+            Self::Add => float_stack
+                .top2()
+                .map_err(Into::<PushInstructionError>::into)
+                .map(|(&x, &y)| x + y)
+                .with_stack_replace(2, state),
+
+            Self::Subtract => float_stack
+                .top2()
+                .map_err(Into::<PushInstructionError>::into)
+                .map(|(&x, &y)| x - y)
+                .with_stack_replace(2, state),
+
+            Self::Multiply => float_stack
+                .top2()
+                .map_err(Into::<PushInstructionError>::into)
+                .map(|(x, y)| (*x) * (*y))
+                .with_stack_replace(2, state),
+
+            Self::ProtectedDivide => float_stack
+                .top2()
+                .map_err(Into::<PushInstructionError>::into)
+                .map(|(&x, &y)| if y == 0.0 { OrderedFloat(1.0) } else { x / y })
+                .with_stack_replace(2, state),
         }
     }
 }
