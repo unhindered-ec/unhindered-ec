67d9227425f1950885fe172918db7d42776c4023
Nic McPhee <mcphee@morris.umn.edu>
2023-12-02 15:56:56 -0600

    Refactored floating point instructions
    This introduces `binary_arithmetic` and `binary_predicate`, which capture the shared logic for two major classes of floating point instructions.
    
    These can be generalized further to be generic in their numeric type so we can use them in integer instructions as well, but that's for later.

--- packages/push/src/instruction/float.rs
+++ packages/push/src/instruction/float.rs
@@ -37,78 +37,33 @@ where
 
     fn perform(&self, mut state: S) -> InstructionResult<S, Self::Error> {
         match self {
-            Self::Push(_) | Self::Add | Self::Subtract | Self::Multiply | Self::ProtectedDivide => {
-                // All these instructions pop at least one value from the float stack, so we're
-                // guaranteed that there will be space for the result. So we don't have to check that
-                // any stacks are full before we start.
-                let float_stack = state.stack_mut::<OrderedFloat<f64>>();
-                match self {
-                    Self::Push(f) => state.with_push(*f).map_err_into(),
+            Self::Push(f) => state.with_push(*f).map_err_into(),
 
-                    Self::Add => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(&x, &y)| x + y)
-                        .with_stack_replace(2, state),
-
-                    Self::Subtract => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(&x, &y)| x - y)
-                        .with_stack_replace(2, state),
-
-                    Self::Multiply => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(x, y)| (*x) * (*y))
-                        .with_stack_replace(2, state),
-
-                    Self::ProtectedDivide => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(&x, &y)| if y == 0.0 { OrderedFloat(1.0) } else { x / y })
-                        .with_stack_replace(2, state),
-
-                    _ => {
-                        unreachable!(
-                            "We failed to handle an arithmetic float instruction: {self:?}"
-                        )
-                    }
-                }
+            // All these instructions pop at least one value from the float stack, so we're
+            // guaranteed that there will be space for the result. So we don't have to check that
+            // any stacks are full before we start.
+            Self::Add => Self::binary_arithmetic(state, std::ops::Add::add),
+            Self::Subtract => Self::binary_arithmetic(state, std::ops::Sub::sub),
+            Self::Multiply => Self::binary_arithmetic(state, std::ops::Mul::mul),
+            Self::ProtectedDivide => {
+                Self::binary_arithmetic(
+                    state,
+                    |x, y| {
+                        if y == 0.0 {
+                            OrderedFloat(1.0)
+                        } else {
+                            x / y
+                        }
+                    },
+                )
             }
 
-            Self::Equal | Self::NotEqual => {
-                // None of these instructions pop anything off the boolean stack, but
-                // they will push a result onto that stack. Thus before we start performing
-                // the instruction, we need to check for the case that the boolean stack is
-                // already full, and return an `Overflow` error if it is.
-                if state.stack::<bool>().is_full() {
-                    return Err(Error::fatal(
-                        state,
-                        StackError::Overflow { stack_type: "bool" },
-                    ));
-                }
-                let float_stack: &mut Stack<OrderedFloat<f64>> =
-                    state.stack_mut::<OrderedFloat<f64>>();
-
-                match self {
-                    Self::Equal => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(&x, &y)| x == y)
-                        .with_stack_push(state)
-                        .with_stack_discard::<OrderedFloat<f64>>(1),
-                    Self::NotEqual => float_stack
-                        .top2()
-                        .map_err(Into::<PushInstructionError>::into)
-                        .map(|(&x, &y)| x != y)
-                        .with_stack_push(state)
-                        .with_stack_discard::<OrderedFloat<f64>>(1),
-                    _ => unreachable!(
-                        "We failed to implement a boolean-valued operation on floats: {self:?}"
-                    ),
-                }
-            }
+            // None of these instructions pop anything off the boolean stack, but
+            // they will push a result onto that stack. Thus before we start performing
+            // the instruction, we need to check for the case that the boolean stack is
+            // already full, and return an `Overflow` error if it is.
+            Self::Equal => Self::binary_predicate(state, std::cmp::PartialEq::eq),
+            Self::NotEqual => Self::binary_predicate(state, std::cmp::PartialEq::ne),
 
             Self::Dup => {
                 if state.stack::<OrderedFloat<f64>>().is_full() {
@@ -128,5 +83,44 @@ where
                     .with_stack_push(state)
             }
         }
+    }
+}
+
+impl FloatInstruction {
+    fn binary_arithmetic<S>(
+        mut state: S,
+        op: impl FnOnce(OrderedFloat<f64>, OrderedFloat<f64>) -> OrderedFloat<f64>,
+    ) -> Result<S, Error<S, PushInstructionError>>
+    where
+        S: Clone + HasStack<OrderedFloat<f64>>,
+    {
+        let float_stack = state.stack_mut::<OrderedFloat<f64>>();
+        float_stack
+            .top2()
+            .map_err(Into::<PushInstructionError>::into)
+            .map(|(&x, &y)| op(x, y))
+            .with_stack_replace(2, state)
+    }
+
+    fn binary_predicate<S>(
+        mut state: S,
+        op: impl FnOnce(&OrderedFloat<f64>, &OrderedFloat<f64>) -> bool,
+    ) -> Result<S, Error<S, PushInstructionError>>
+    where
+        S: Clone + HasStack<OrderedFloat<f64>> + HasStack<bool>,
+    {
+        if state.stack::<bool>().is_full() {
+            return Err(Error::fatal(
+                state,
+                StackError::Overflow { stack_type: "bool" },
+            ));
+        }
+        let float_stack: &mut Stack<OrderedFloat<f64>> = state.stack_mut::<OrderedFloat<f64>>();
+        float_stack
+            .top2()
+            .map_err(Into::<PushInstructionError>::into)
+            .map(|(x, y)| op(x, y))
+            .with_stack_push(state)
+            .with_stack_discard::<OrderedFloat<f64>>(1)
     }
 }
