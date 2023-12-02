b8a676db7b412c98be29975f9e1ee16289810062
Nic McPhee <mcphee@morris.umn.edu>
2023-12-01 22:26:10 -0600

    Implement `Equal` and `NotEqual` for floats
    This adds `Equal` and `NotEqual` for floating point values, and tests.
    
    I also added a test that confirms that we return a `StackError::Overflow` when comparing two floats pushes a boolean onto a full boolean stack.

--- packages/push/src/instruction/float.rs
+++ packages/push/src/instruction/float.rs
@@ -1,8 +1,11 @@
 use super::{Instruction, PushInstruction, PushInstructionError};
 use crate::{
-    error::InstructionResult,
+    error::{Error, InstructionResult},
     instruction::MapInstructionError,
-    push_vm::{stack::StackPush, HasStack},
+    push_vm::{
+        stack::{Stack, StackDiscard, StackError, StackPush},
+        HasStack,
+    },
 };
 use ordered_float::OrderedFloat;
 use strum_macros::EnumIter;
@@ -15,6 +18,8 @@ pub enum FloatInstruction {
     Subtract,
     Multiply,
     ProtectedDivide,
+    Equal,
+    NotEqual,
 }
 
 impl From<FloatInstruction> for PushInstruction {
@@ -25,38 +30,84 @@ impl From<FloatInstruction> for PushInstruction {
 
 impl<S> Instruction<S> for FloatInstruction
 where
-    S: Clone + HasStack<OrderedFloat<f64>>,
+    S: Clone + HasStack<OrderedFloat<f64>> + HasStack<bool>,
 {
     type Error = PushInstructionError;
 
     fn perform(&self, mut state: S) -> InstructionResult<S, Self::Error> {
-        let float_stack = state.stack_mut::<OrderedFloat<f64>>();
         match self {
-            Self::Push(f) => state.with_push(*f).map_err_into(),
+            Self::Push(_) | Self::Add | Self::Subtract | Self::Multiply | Self::ProtectedDivide => {
+                // All these instructions pop at least one value from the float stack, so we're
+                // guaranteed that there will be space for the result. So we don't have to check that
+                // any stacks are full before we start.
+                let float_stack = state.stack_mut::<OrderedFloat<f64>>();
+                match self {
+                    Self::Push(f) => state.with_push(*f).map_err_into(),
 
-            Self::Add => float_stack
-                .top2()
-                .map_err(Into::<PushInstructionError>::into)
-                .map(|(&x, &y)| x + y)
-                .with_stack_replace(2, state),
+                    Self::Add => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(&x, &y)| x + y)
+                        .with_stack_replace(2, state),
 
-            Self::Subtract => float_stack
-                .top2()
-                .map_err(Into::<PushInstructionError>::into)
-                .map(|(&x, &y)| x - y)
-                .with_stack_replace(2, state),
+                    Self::Subtract => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(&x, &y)| x - y)
+                        .with_stack_replace(2, state),
 
-            Self::Multiply => float_stack
-                .top2()
-                .map_err(Into::<PushInstructionError>::into)
-                .map(|(x, y)| (*x) * (*y))
-                .with_stack_replace(2, state),
+                    Self::Multiply => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(x, y)| (*x) * (*y))
+                        .with_stack_replace(2, state),
 
-            Self::ProtectedDivide => float_stack
-                .top2()
-                .map_err(Into::<PushInstructionError>::into)
-                .map(|(&x, &y)| if y == 0.0 { OrderedFloat(1.0) } else { x / y })
-                .with_stack_replace(2, state),
+                    Self::ProtectedDivide => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(&x, &y)| if y == 0.0 { OrderedFloat(1.0) } else { x / y })
+                        .with_stack_replace(2, state),
+
+                    _ => {
+                        unreachable!(
+                            "We failed to handle an arithmetic float instruction: {self:?}"
+                        )
+                    }
+                }
+            }
+
+            Self::Equal | Self::NotEqual => {
+                // None of these instructions pop anything off the boolean stack, but
+                // they will push a result onto that stack. Thus before we start performing
+                // the instruction, we need to check for the case that the boolean stack is
+                // already full, and return an `Overflow` error if it is.
+                if state.stack::<bool>().is_full() {
+                    return Err(Error::fatal(
+                        state,
+                        StackError::Overflow { stack_type: "bool" },
+                    ));
+                }
+                let float_stack: &mut Stack<OrderedFloat<f64>> =
+                    state.stack_mut::<OrderedFloat<f64>>();
+
+                match self {
+                    Self::Equal => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(&x, &y)| x == y)
+                        .with_stack_push(state)
+                        .with_stack_discard::<OrderedFloat<f64>>(1),
+                    Self::NotEqual => float_stack
+                        .top2()
+                        .map_err(Into::<PushInstructionError>::into)
+                        .map(|(&x, &y)| x != y)
+                        .with_stack_push(state)
+                        .with_stack_discard::<OrderedFloat<f64>>(1),
+                    _ => unreachable!(
+                        "We failed to implement a boolean-valued operation on floats: {self:?}"
+                    ),
+                }
+            }
         }
     }
 }
--- packages/push/tests/float.rs
+++ packages/push/tests/float.rs
@@ -2,7 +2,7 @@ use ordered_float::OrderedFloat;
 use proptest::{prop_assert_eq, proptest};
 use push::{
     instruction::{FloatInstruction, Instruction, PushInstruction},
-    push_vm::{push_state::PushState, HasStack},
+    push_vm::{push_state::PushState, stack::StackError, HasStack},
 };
 
 #[test]
@@ -39,6 +39,26 @@ fn add() {
     let result = FloatInstruction::Add.perform(state).unwrap();
     assert_eq!(result.stack::<OrderedFloat<f64>>().size(), 1);
     assert_eq!(*result.stack::<OrderedFloat<f64>>().top().unwrap(), x + y);
+}
+
+#[test]
+fn overflow_bool_stack() {
+    let x = OrderedFloat(409.37);
+    let mut state = PushState::builder()
+        .with_max_stack_size(2)
+        .with_program([])
+        .unwrap()
+        .build();
+    state.stack_mut::<OrderedFloat<f64>>().push(x).unwrap();
+    state.stack_mut::<OrderedFloat<f64>>().push(x).unwrap();
+    // Now fill the boolean stack so that it overflows when we check the floats for equality
+    state.stack_mut::<bool>().push(false).unwrap();
+    state.stack_mut::<bool>().push(false).unwrap();
+    let result = FloatInstruction::Equal.perform(state).unwrap_err();
+    assert_eq!(
+        result.error(),
+        &StackError::Overflow { stack_type: "bool" }.into()
+    );
 }
 
 proptest! {
@@ -99,6 +119,36 @@ proptest! {
         state.stack_mut::<OrderedFloat<f64>>().push(OrderedFloat(x)).unwrap();
         let result = FloatInstruction::ProtectedDivide.perform(state).unwrap();
         let output = result.stack::<OrderedFloat<f64>>().top().unwrap();
+        prop_assert_eq!(*output, expected_result);
+    }
+
+    #[test]
+    fn equal_prop(x in proptest::num::f64::ANY, y in proptest::num::f64::ANY) {
+        let expected_result = OrderedFloat(x) == OrderedFloat(y);
+        let mut state = PushState::builder()
+            .with_max_stack_size(100)
+            .with_program([])
+            .unwrap()
+            .build();
+        state.stack_mut::<OrderedFloat<f64>>().push(OrderedFloat(y)).unwrap();
+        state.stack_mut::<OrderedFloat<f64>>().push(OrderedFloat(x)).unwrap();
+        let result = FloatInstruction::Equal.perform(state).unwrap();
+        let output = result.stack::<bool>().top().unwrap();
+        prop_assert_eq!(*output, expected_result);
+    }
+
+    #[test]
+    fn not_equal_prop(x in proptest::num::f64::ANY, y in proptest::num::f64::ANY) {
+        let expected_result = OrderedFloat(x) != OrderedFloat(y);
+        let mut state = PushState::builder()
+            .with_max_stack_size(100)
+            .with_program([])
+            .unwrap()
+            .build();
+        state.stack_mut::<OrderedFloat<f64>>().push(OrderedFloat(y)).unwrap();
+        state.stack_mut::<OrderedFloat<f64>>().push(OrderedFloat(x)).unwrap();
+        let result = FloatInstruction::NotEqual.perform(state).unwrap();
+        let output = result.stack::<bool>().top().unwrap();
         prop_assert_eq!(*output, expected_result);
     }
 }
