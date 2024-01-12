8137441888212357b386b71736694306de72a35d
Nic McPhee <mcphee@morris.umn.edu>
2024-01-12 08:59:00 -0600

    Fix documentation comment symbols
    I had used `//` in a couple of places where I should have used `///`.
    
    Co-authored-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/push/src/instruction/instruction_error.rs
+++ packages/push/src/instruction/instruction_error.rs
@@ -4,12 +4,12 @@ use crate::push_vm::stack::StackError;
 /// An error that can occur when performing a `PushInstruction`.
 #[derive(thiserror::Error, Debug, Eq, PartialEq)]
 pub enum PushInstructionError {
-    // Stack errors can be things like stack over- or underflows.
+    /// Stack errors can be things like stack over- or underflows.
     #[error(transparent)]
     StackError(#[from] StackError),
     #[error("Exceeded the maximum step limit {step_limit}")]
     StepLimitExceeded { step_limit: usize },
-    // Int errors can be things like integer overflows.
+    /// Int errors can be things like integer overflows.
     #[error(transparent)]
     Int(#[from] IntInstructionError),
 }
