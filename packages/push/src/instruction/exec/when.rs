a9716a6d7b4b5a29b0baf661b4233922dd80fe2f
Nic McPhee <mcphee@morris.umn.edu>
2024-04-28 11:40:28 -0500

    Document and fix `When` instruction
    This adds fairly detailed documentation for the `When` instruction that we think makes a reasonable starting point as a general instruction documentation template.
    
    Among other things, this lists all five cases for the `When` instruction.
    
    In doing this we decided that we should return an underflow error when both the boolean and exec stacks are empty. We then updated the relevant test to confirm that behavior, and then updated the `.perform()` method to get that updated test to pass.

--- packages/push/src/instruction/exec/when.rs
+++ packages/push/src/instruction/exec/when.rs
@@ -8,6 +8,51 @@ use crate::{
     },
 };
 
+/// The Push `When` instruction performs a block of code on the
+/// `Exec` stack _when_ the boolean on the top of the `bool` stack is
+/// true, ignoring that block otherwise.
+///
+/// # Inputs
+///
+/// The `When` instruction takes the following inputs:
+///    - `Bool` stack
+///      - Zero or one booleans
+///    - `Exec` stack
+///      - Zero or one code blocks
+///
+/// # Behavior
+///
+/// An important feature of `When` is the code block is only executed when:
+///    - There is a boolean condition, and
+///    - That condition is `true`
+///
+/// ## Action Table
+///
+/// The table below indicates the behavior in each of the different
+/// cases.
+///
+///    - The "Boolean stack" column indicates the value of the top of the
+///      boolean stack, or whether it exists
+///    - The "Code block" column indicates whether there is a code block on the
+///      `Exec stack`.
+///    - The "Action" columns indicate the action taken on the respective
+/// stacks.
+///       - "Consumed" means the value on that stack is consumed (i.e., removed)
+///       - "Unchanged" means that value was left on the stack unchanged.
+///
+/// | Boolean stack  | Code block | Action (`Bool`) | Action (Code block) | Returns error | Note |
+/// | ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |
+/// | `true`  | exists | Consumed | Unchanged | ✖️ | The code block is executed |
+/// | `false` | exists | Consumed | Consumed | ✖️ | The code block is skipped |
+/// | missing | exists | Non-existent | Consumed | ✖️ | The code block is skipped |
+/// | exists | missing | Unchanged | Non-existent | ✖️ | No action is taken; state is unchanged |
+/// | missing | missing | Non-existent | Non-existent | ❗ | No action is take; a recoverable [`StackError::Underflow`] error is returned |
+///
+/// # Errors
+///
+/// If either of the stack accesses returns any error other than a
+/// [`StackError::Underflow`] then this returns that as a [`Error::Fatal`]
+/// error.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct When;
 
@@ -43,12 +88,16 @@ where
             (Err(StackError::Underflow { .. }), Ok(_)) => {
                 Ok(state).with_stack_discard::<PushProgram>(1)
             }
-            // If there is no block, then we just skip this instruction regardless
-            // of whether there's a boolean or not.
-            (Ok(_) | Err(StackError::Underflow { .. }), Err(StackError::Underflow { .. })) => {
-                Ok(state)
+            // If there is a boolean but no block, then we just skip this instruction.
+            (Ok(_), Err(StackError::Underflow { .. })) => Ok(state),
+            // If there is no boolean and no block, then we return an stack underflow
+            // error.
+            (Err(StackError::Underflow { .. }), Err(e @ StackError::Underflow { .. })) => {
+                Err(Error::recoverable(state, e))
             }
-            (Err(e), _) | (_, Err(e)) => Err(Error::recoverable(state, e)),
+            // If some other error occurs (e.g., a fatal error), then we just
+            // pass that forward.
+            (Err(e), _) | (_, Err(e)) => Err(Error::fatal(state, e)),
         }
     }
 }
@@ -57,8 +106,9 @@ where
 #[allow(clippy::unwrap_used)]
 mod tests {
     use crate::{
-        instruction::{exec::when::When, ExecInstruction, Instruction},
-        push_vm::push_state::PushState,
+        error::into_state::IntoState,
+        instruction::{exec::when::When, ExecInstruction, Instruction, PushInstructionError},
+        push_vm::{push_state::PushState, stack::StackError},
     };
 
     #[test]
@@ -69,16 +119,33 @@ mod tests {
             .with_bool_values([])
             .unwrap()
             .build();
+        let result_error = When.perform(state.clone()).unwrap_err();
+        assert!(result_error.is_recoverable());
+        assert!(matches!(
+            result_error.error(),
+            PushInstructionError::StackError(StackError::Underflow { .. })
+        ));
+        assert_eq!(result_error.into_state(), state);
+    }
+
+    #[test]
+    fn when_is_correct_with_true_and_empty_exec() {
+        let state = PushState::builder()
+            .with_max_stack_size(1000)
+            .with_no_program()
+            .with_bool_values([true])
+            .unwrap()
+            .build();
         let result_state = When.perform(state.clone()).unwrap();
         assert_eq!(result_state, state);
     }
 
     #[test]
-    fn when_is_correct_with_empty_exec() {
+    fn when_is_correct_with_false_and_empty_exec() {
         let state = PushState::builder()
             .with_max_stack_size(1000)
             .with_no_program()
-            .with_bool_values([true])
+            .with_bool_values([false])
             .unwrap()
             .build();
         let result_state = When.perform(state.clone()).unwrap();
