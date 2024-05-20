fb759fb1ecbe7fe4798d3ccfc412329bcf2d1040
Nic McPhee <mcphee@morris.umn.edu>
2024-05-20 17:49:46 -0500

    Add failing test for `Negate`
    This captures the fact that `Negate` doesn't work the way we'd like when the value being negated is `i64::MIN`.

--- packages/push/src/instruction/int/negate.rs
+++ packages/push/src/instruction/int/negate.rs
@@ -33,6 +33,24 @@ mod tests {
         push_vm::{push_state::PushState, HasStack},
     };
 
+    // We need to make sure `Negate` properly handles the
+    // case where the value being negated is `i64::MIN`.
+    // Simply negating that value will wrap around to
+    // `i64::MAX_VALUE`, which is definitely not what we want.
+    #[test]
+    fn handle_min() {
+        let input = i64::MIN;
+        let state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_int_values(std::iter::once(input))
+            .unwrap()
+            .with_no_program()
+            .build();
+        let result = IntInstruction::negate().perform(state).unwrap();
+        assert_eq!(result.stack::<i64>().size(), 1);
+        assert_eq!(*result.stack::<i64>().top().unwrap(), i64::MAX);
+    }
+
     #[proptest]
     fn negate(#[any] x: i64) {
         let state = PushState::builder()
