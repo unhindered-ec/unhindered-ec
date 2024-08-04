6ca0be660723bc8814f95cc0b46542085481ed7a
Nic McPhee <mcphee@morris.umn.edu>
2024-08-04 11:54:18 -0500

    Move `impl Cases` block back to `mod.rs`
    This puts all the direct `impl` blocks on `Cases` into `mod.rs`, leaving just trait implementations in `iter.rs`.
    
    This keeps the order of methods in the generated documentation reasonable, having the `impl Cases` methods first and all the trait implementations after that.

--- packages/push/src/evaluation/cases/iter.rs
+++ packages/push/src/evaluation/cases/iter.rs
@@ -1,167 +1,7 @@
+//! A collection of implementations converting `Cases` into various iterators
+//! and constructing them from iterators.
+
 use super::super::{Case, Cases};
-
-// A collection of iterators on `Cases`.
-
-impl<Input, Output> Cases<Input, Output> {
-    /// Creates an iterator over the set of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::{Case, Cases, WithTargetFn};
-    /// #
-    /// let inputs = ["Hello", "People"];
-    /// let cases = inputs.with_target_fn(|s| s.len());
-    ///
-    /// let mut iter = cases.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&Case::new("Hello", 5)));
-    /// assert_eq!(iter.next(), Some(&Case::new("People", 6)));
-    /// assert_eq!(iter.next(), None);
-    /// ```
-    pub fn iter(&self) -> impl Iterator<Item = &Case<Input, Output>> {
-        // std::slice::Iter<Case<Input, Output>> {
-        self.cases.iter()
-    }
-
-    /// Creates a iterator over mutable instances of the cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::{Case, Cases};
-    /// #
-    /// let first_case = Case::new("Hello", 5);
-    /// let mut second_case = Case::new("People", 6);
-    /// let mut cases = Cases::new().with_case(first_case).with_case(second_case);
-    ///
-    /// for c in cases.iter_mut() {
-    ///     (*c).output *= 2;
-    /// }
-    ///
-    /// let mut iter = cases.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&Case::new("Hello", 10)));
-    /// assert_eq!(iter.next(), Some(&Case::new("People", 12)));
-    /// assert_eq!(iter.next(), None);
-    /// ```
-    pub fn iter_mut(&mut self) -> std::slice::IterMut<Case<Input, Output>> {
-        self.cases.iter_mut()
-    }
-
-    /// Creates an iterator over the inputs for this set of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs = ["this", "and", "those"];
-    /// let cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// assert!(cases.inputs().eq(&inputs));
-    /// ```
-    pub fn inputs(&self) -> impl Iterator<Item = &Input> {
-        self.cases.iter().map(|c| &c.input)
-    }
-
-    /// Creates an iterator over mutable references to the inputs for this set
-    /// of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs: Vec<String> = ["this", "and", "those"]
-    ///     .iter()
-    ///     .map(|s| s.to_string())
-    ///     .collect();
-    /// let mut cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// for input in cases.inputs_mut() {
-    ///     *input = input.to_uppercase();
-    /// }
-    ///
-    /// assert!(cases.inputs().eq(&["THIS", "AND", "THOSE"]));
-    /// assert!(cases.outputs().eq(&[4, 3, 5]));
-    /// ```
-    pub fn inputs_mut(&mut self) -> impl Iterator<Item = &mut Input> {
-        self.cases.iter_mut().map(|c| &mut c.input)
-    }
-
-    /// Convert this set of cases into an iterator over the inputs. This
-    /// takes ownership of the cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs = ["this", "and", "those"];
-    /// let cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// assert!(cases.into_inputs().eq(["this", "and", "those"]));
-    /// ```
-    pub fn into_inputs(self) -> impl Iterator<Item = Input> {
-        self.cases.into_iter().map(|c| c.input)
-    }
-
-    /// Creates an iterator over the outputs for this set of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs = ["this", "and", "those"];
-    /// let cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// assert!(cases.outputs().eq(&[4, 3, 5]));
-    /// ```
-    pub fn outputs(&self) -> impl Iterator<Item = &Output> {
-        self.cases.iter().map(|c| &c.output)
-    }
-
-    /// Creates an iterator over mutable references to the outputs for this set
-    /// of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs = ["this", "and", "those"];
-    /// let mut cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// for output in cases.outputs_mut() {
-    ///     *output *= 2;
-    /// }
-    ///
-    /// assert!(cases.inputs().eq(&["this", "and", "those"]));
-    /// assert!(cases.outputs().eq(&[8, 6, 10]));
-    /// ```
-    pub fn outputs_mut(&mut self) -> impl Iterator<Item = &mut Output> {
-        self.cases.iter_mut().map(|c| &mut c.output)
-    }
-
-    /// Convert this set of cases into an iterator over the outputs. This
-    /// takes ownership of the cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let inputs = ["this", "and", "those"];
-    /// let cases = Cases::from_inputs(inputs, |s| s.len());
-    ///
-    /// assert!(cases.into_outputs().eq([4, 3, 5]));
-    /// ```
-    pub fn into_outputs(self) -> impl Iterator<Item = Output> {
-        self.cases.into_iter().map(|c| c.output)
-    }
-}
 
 impl<Input, Output> IntoIterator for Cases<Input, Output> {
     type Item = Case<Input, Output>;
@@ -246,5 +86,30 @@ impl<'a, Input, Output> IntoIterator for &'a mut Cases<Input, Output> {
     /// ```
     fn into_iter(self) -> Self::IntoIter {
         self.cases.iter_mut()
+    }
+}
+
+impl<Input, Output, C> FromIterator<C> for Cases<Input, Output>
+where
+    C: Into<Case<Input, Output>>,
+{
+    /// Create a set of cases from an iterator over items that can
+    /// be converted into individual cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::{Case, Cases};
+    /// #
+    /// let items = [("Hello", 5), ("People", 6)];
+    /// let cases = Cases::from_iter(items);
+    ///
+    /// assert!(cases.inputs().eq(&["Hello", "People"]));
+    /// assert!(cases.outputs().eq(&[5, 6]));
+    /// ```
+    fn from_iter<T: IntoIterator<Item = C>>(iter: T) -> Self {
+        Self {
+            cases: iter.into_iter().map(Into::into).collect(),
+        }
     }
 }
--- packages/push/src/evaluation/cases/mod.rs
+++ packages/push/src/evaluation/cases/mod.rs
@@ -28,27 +28,6 @@ pub struct Cases<Input, Output = Input> {
     cases: Vec<Case<Input, Output>>,
 }
 
-// --------------------
-// CREATE SETS OF CASES
-// --------------------
-
-impl<Input, Output> Default for Cases<Input, Output> {
-    /// Create an empty set of cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::Cases;
-    /// #
-    /// let cases = Cases::<String, usize>::default();
-    ///
-    /// assert!(cases.is_empty());
-    /// ```
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl<Input, Output> Cases<Input, Output> {
     /// Create an empty set of cases.
     ///
@@ -127,38 +106,7 @@ impl<Input, Output> Cases<Input, Output> {
         self.add_case(case);
         self
     }
-}
 
-impl<Input, Output, C> FromIterator<C> for Cases<Input, Output>
-where
-    C: Into<Case<Input, Output>>,
-{
-    /// Create a set of cases from an iterator over items that can
-    /// be converted into individual cases.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # use push::evaluation::{Case, Cases};
-    /// #
-    /// let items = [("Hello", 5), ("People", 6)];
-    /// let cases = Cases::from_iter(items);
-    ///
-    /// assert!(cases.inputs().eq(&["Hello", "People"]));
-    /// assert!(cases.outputs().eq(&[5, 6]));
-    /// ```
-    fn from_iter<T: IntoIterator<Item = C>>(iter: T) -> Self {
-        Self {
-            cases: iter.into_iter().map(Into::into).collect(),
-        }
-    }
-}
-
-// -------------------
-// PROPERTIES OF CASES
-// -------------------
-
-impl<Input, Output> Cases<Input, Output> {
     /// Determine if this set of `Cases` is empty.
     ///
     /// # Examples
@@ -190,6 +138,185 @@ impl<Input, Output> Cases<Input, Output> {
     #[must_use]
     pub fn len(&self) -> usize {
         self.cases.len()
+    }
+}
+
+/// Implement various iterators over `Cases`, inputs and outputs.
+impl<Input, Output> Cases<Input, Output> {
+    /// Creates an iterator over the set of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::{Case, Cases, WithTargetFn};
+    /// #
+    /// let inputs = ["Hello", "People"];
+    /// let cases = inputs.with_target_fn(|s| s.len());
+    ///
+    /// let mut iter = cases.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&Case::new("Hello", 5)));
+    /// assert_eq!(iter.next(), Some(&Case::new("People", 6)));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    pub fn iter(&self) -> impl Iterator<Item = &Case<Input, Output>> {
+        // std::slice::Iter<Case<Input, Output>> {
+        self.cases.iter()
+    }
+
+    /// Creates a iterator over mutable instances of the cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::{Case, Cases};
+    /// #
+    /// let first_case = Case::new("Hello", 5);
+    /// let mut second_case = Case::new("People", 6);
+    /// let mut cases = Cases::new().with_case(first_case).with_case(second_case);
+    ///
+    /// for c in cases.iter_mut() {
+    ///     (*c).output *= 2;
+    /// }
+    ///
+    /// let mut iter = cases.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&Case::new("Hello", 10)));
+    /// assert_eq!(iter.next(), Some(&Case::new("People", 12)));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<Case<Input, Output>> {
+        self.cases.iter_mut()
+    }
+
+    /// Creates an iterator over the inputs for this set of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs = ["this", "and", "those"];
+    /// let cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// assert!(cases.inputs().eq(&inputs));
+    /// ```
+    pub fn inputs(&self) -> impl Iterator<Item = &Input> {
+        self.cases.iter().map(|c| &c.input)
+    }
+
+    /// Creates an iterator over mutable references to the inputs for this set
+    /// of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs: Vec<String> = ["this", "and", "those"]
+    ///     .iter()
+    ///     .map(|s| s.to_string())
+    ///     .collect();
+    /// let mut cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// for input in cases.inputs_mut() {
+    ///     *input = input.to_uppercase();
+    /// }
+    ///
+    /// assert!(cases.inputs().eq(&["THIS", "AND", "THOSE"]));
+    /// assert!(cases.outputs().eq(&[4, 3, 5]));
+    /// ```
+    pub fn inputs_mut(&mut self) -> impl Iterator<Item = &mut Input> {
+        self.cases.iter_mut().map(|c| &mut c.input)
+    }
+
+    /// Convert this set of cases into an iterator over the inputs. This
+    /// takes ownership of the cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs = ["this", "and", "those"];
+    /// let cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// assert!(cases.into_inputs().eq(["this", "and", "those"]));
+    /// ```
+    pub fn into_inputs(self) -> impl Iterator<Item = Input> {
+        self.cases.into_iter().map(|c| c.input)
+    }
+
+    /// Creates an iterator over the outputs for this set of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs = ["this", "and", "those"];
+    /// let cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// assert!(cases.outputs().eq(&[4, 3, 5]));
+    /// ```
+    pub fn outputs(&self) -> impl Iterator<Item = &Output> {
+        self.cases.iter().map(|c| &c.output)
+    }
+
+    /// Creates an iterator over mutable references to the outputs for this set
+    /// of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs = ["this", "and", "those"];
+    /// let mut cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// for output in cases.outputs_mut() {
+    ///     *output *= 2;
+    /// }
+    ///
+    /// assert!(cases.inputs().eq(&["this", "and", "those"]));
+    /// assert!(cases.outputs().eq(&[8, 6, 10]));
+    /// ```
+    pub fn outputs_mut(&mut self) -> impl Iterator<Item = &mut Output> {
+        self.cases.iter_mut().map(|c| &mut c.output)
+    }
+
+    /// Convert this set of cases into an iterator over the outputs. This
+    /// takes ownership of the cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let inputs = ["this", "and", "those"];
+    /// let cases = Cases::from_inputs(inputs, |s| s.len());
+    ///
+    /// assert!(cases.into_outputs().eq([4, 3, 5]));
+    /// ```
+    pub fn into_outputs(self) -> impl Iterator<Item = Output> {
+        self.cases.into_iter().map(|c| c.output)
+    }
+}
+
+impl<Input, Output> Default for Cases<Input, Output> {
+    /// Create an empty set of cases.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use push::evaluation::Cases;
+    /// #
+    /// let cases = Cases::<String, usize>::default();
+    ///
+    /// assert!(cases.is_empty());
+    /// ```
+    fn default() -> Self {
+        Self::new()
     }
 }
 
