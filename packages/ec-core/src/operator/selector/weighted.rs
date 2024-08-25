bbe36beed63fd8db995d4245c6421395c2e09013
Nic McPhee <mcphee@morris.umn.edu>
2024-08-25 10:32:08 -0500

    Add `SelectionError` to `Weighted` selection
    This led to some pretty complicated types with nested boxing, which took a while to get working. We might want to rethink our implementation here.

--- packages/ec-core/src/operator/selector/weighted.rs
+++ packages/ec-core/src/operator/selector/weighted.rs
@@ -1,11 +1,33 @@
-use anyhow::{Context, Result};
-use rand::{rngs::ThreadRng, seq::IndexedRandom};
+use std::error::Error;
 
-use super::Selector;
+use miette::Diagnostic;
+use rand::{
+    rngs::ThreadRng,
+    seq::{IndexedRandom, WeightError},
+};
+
+use super::{error::SelectionError, Selector};
 use crate::population::Population;
 
 pub struct Weighted<P: Population> {
-    selectors: Vec<(Box<dyn Selector<P> + Send + Sync>, usize)>,
+    selectors: Vec<(
+        Box<dyn Selector<P, Error = Box<dyn Error>> + Send + Sync>,
+        usize,
+    )>,
+}
+
+#[derive(Debug, thiserror::Error, Diagnostic)]
+pub enum WeightedError {
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    Selector(#[from] SelectionError),
+
+    #[error(transparent)]
+    #[diagnostic(help = "Ensure that the weights are all non-negative and add to more than zero")]
+    Weight(#[from] WeightError),
+
+    #[error(transparent)]
+    Other(Box<dyn Error>),
 }
 
 impl<P: Population> Weighted<P> {
@@ -15,7 +37,10 @@ impl<P: Population> Weighted<P> {
     #[must_use]
     pub fn new<S>(selector: S, weight: usize) -> Self
     where
-        S: Selector<P> + Send + Sync + 'static,
+        S: Selector<P, Error = std::boxed::Box<(dyn std::error::Error + 'static)>>
+            + Send
+            + Sync
+            + 'static,
     {
         Self {
             selectors: vec![(Box::new(selector), weight)],
@@ -25,7 +50,10 @@ impl<P: Population> Weighted<P> {
     #[must_use]
     pub fn with_selector<S>(mut self, selector: S, weight: usize) -> Self
     where
-        S: Selector<P> + Send + Sync + 'static,
+        S: Selector<P, Error = std::boxed::Box<(dyn std::error::Error + 'static)>>
+            + Send
+            + Sync
+            + 'static,
     {
         self.selectors.push((Box::new(selector), weight));
         self
@@ -36,16 +64,36 @@ impl<P> Selector<P> for Weighted<P>
 where
     P: Population,
 {
+    type Error = WeightedError;
+
     fn select<'pop>(
         &self,
         population: &'pop P,
         rng: &mut ThreadRng,
-    ) -> Result<&'pop P::Individual> {
-        let (selector, _) = self
-            .selectors
-            .choose_weighted(rng, |(_, w)| *w)
-            .context("The set of selectors was empty")?;
-        selector.select(population, rng)
+    ) -> Result<&'pop P::Individual, Self::Error> {
+        let (selector, _) = self.selectors.choose_weighted(rng, |(_, w)| *w)?;
+        selector
+            .select(population, rng)
+            .map_err(WeightedError::Other)
+    }
+}
+
+impl<P, S> Selector<P> for Box<S>
+where
+    P: Population,
+    S: Selector<P>,
+    S::Error: Error + 'static,
+{
+    type Error = Box<dyn Error>;
+
+    fn select<'pop>(
+        &self,
+        population: &'pop P,
+        rng: &mut ThreadRng,
+    ) -> Result<&'pop <P as Population>::Individual, Self::Error> {
+        (**self)
+            .select(population, rng)
+            .map_err(|e| Box::new(e).into())
     }
 }
 
@@ -70,7 +118,7 @@ mod tests {
         let mut rng = rand::thread_rng();
         // We'll make a selector that has a 50/50 chance of choosing the highest
         // or lowest value.
-        let weighted = Weighted::new(Best, 1).with_selector(Worst, 1);
+        let weighted = Weighted::new(Box::new(Best), 1).with_selector(Box::new(Worst), 1);
         let selection = weighted.select(&pop, &mut rng).unwrap();
         let extremes: [&i32; 2] = pop.iter().minmax().into_option().unwrap().into();
         assert!(extremes.contains(&selection));
