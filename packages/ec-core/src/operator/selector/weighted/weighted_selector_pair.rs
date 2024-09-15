5c33bdf747b2a8f3b49c947e9577f6193b3721d2
Nic McPhee <mcphee@morris.umn.edu>
2024-09-15 12:17:34 -0500

    Move and extend `WeightedSelectorPair` tests
    The tests that were in `mod.rs` were really tests for `WeightedSelectorPair`, so we moved them there. We also then extended the tests to improve the coverage.

--- packages/ec-core/src/operator/selector/weighted/weighted_selector_pair.rs
+++ packages/ec-core/src/operator/selector/weighted/weighted_selector_pair.rs
@@ -94,3 +94,113 @@ impl<A, B> WeightedSelectorPair<A, B> {
         })
     }
 }
+
+#[cfg(test)]
+#[rustversion::attr(before(1.81), allow(clippy::unwrap_used))]
+#[rustversion::attr(
+    since(1.81),
+    expect(
+        clippy::unwrap_used,
+        reason = "Panicking is the best way to deal with errors in unit tests"
+    )
+)]
+mod tests {
+    use itertools::Itertools;
+    use test_strategy::proptest;
+
+    use crate::operator::selector::{
+        best::Best,
+        random::Random,
+        tournament::Tournament,
+        weighted::{
+            weighted::Weighted,
+            weighted_selector_pair::{WeightedSelectorPair, WeightedSelectorsError},
+            with_weighted_selector::WithWeightedSelector,
+            WeightSumOverflow, WithWeight,
+        },
+        worst::Worst,
+        Selector,
+    };
+
+    #[test]
+    fn can_construct_pair() {
+        let weighted = Weighted::new(Best, 5).with_selector_and_weight(Worst, 8);
+        assert!(matches!(
+            weighted,
+            Ok(WeightedSelectorPair {
+                a: Weighted {
+                    selector: Best,
+                    weight: 5
+                },
+                b: Weighted {
+                    selector: Worst,
+                    weight: 8
+                },
+                distr: Some(_),
+                weight_sum: 13,
+            })
+        ));
+        assert_eq!(13, weighted.unwrap().weight());
+    }
+
+    #[proptest]
+    fn best_or_worst(#[map(|v: [i32;10]| v.into())] pop: Vec<i32>) {
+        let mut rng = rand::thread_rng();
+        // We'll make a selector that has a 50/50 chance of choosing the highest
+        // or lowest value.
+        let weighted = Weighted::new(Best, 1)
+            .with_selector_and_weight(Worst, 1)
+            .unwrap();
+        let selection = weighted.select(&pop, &mut rng).unwrap();
+        let extremes: [&i32; 2] = pop.iter().minmax().into_option().unwrap().into();
+        assert!(extremes.contains(&selection));
+    }
+
+    #[proptest]
+    fn several_selectors(#[map(|v: [i32;10]| v.into())] pop: Vec<i32>) {
+        let mut rng = rand::thread_rng();
+        // We'll make a selector that has a 50/50 chance of choosing the highest
+        // or lowest value.
+        let weighted = Weighted::new(Best, 1)
+            .with_selector_and_weight(Worst, 1)
+            .with_selector_and_weight(Random, 0)
+            .with_selector_and_weight(Tournament::of_size::<3>(), 2)
+            .unwrap();
+        let selection = weighted.select(&pop, &mut rng).unwrap();
+        assert!(pop.contains(selection));
+    }
+
+    #[test]
+    fn zero_weight_sum_error() {
+        let pop = vec![5, 8, 9, 6, 3, 2, 0];
+        let mut rng = rand::thread_rng();
+        let weighted = Weighted::new(Best, 0)
+            .with_selector_and_weight(Worst, 0)
+            .with_selector_and_weight(Random, 0)
+            .unwrap();
+        // If all the weights are zero, then selection should return an appropriate
+        // error type.
+        assert_eq!(
+            weighted.select(&pop, &mut rng).unwrap_err(),
+            WeightedSelectorsError::ZeroWeightSum
+        );
+    }
+
+    #[test]
+    fn weight_sum_overflow_error() {
+        let weighted = Weighted::new(Best, u32::MAX).with_selector_and_weight(Worst, 0);
+        assert!(weighted.is_ok());
+        let weighted = weighted
+            .with_selector_and_weight(Random, u32::MAX - 1)
+            .unwrap_err();
+        assert_eq!(weighted, WeightSumOverflow(u32::MAX, u32::MAX - 1));
+    }
+
+    #[test]
+    fn weight_sum_overflow_error_chaining() {
+        let weighted = Weighted::new(Best, u32::MAX).with_selector_and_weight(Random, u32::MAX - 1);
+        assert!(weighted.is_err());
+        let weighted = weighted.with_selector_and_weight(Worst, 0).unwrap_err();
+        assert_eq!(weighted, WeightSumOverflow(u32::MAX, u32::MAX - 1));
+    }
+}
