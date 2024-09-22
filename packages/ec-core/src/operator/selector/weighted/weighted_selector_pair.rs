a013aaf6969f4bf086fa53ea80a83016ba7733ad
Nic McPhee <mcphee@morris.umn.edu>
2024-09-22 10:20:28 -0500

    Implement `miette::Diagnostic` for `WeightedSelectosError`
    This just passes through for the nested error types, and provides a `help` hint for `ZeroWeightedSum`.

--- packages/ec-core/src/operator/selector/weighted/weighted_selector_pair.rs
+++ packages/ec-core/src/operator/selector/weighted/weighted_selector_pair.rs
@@ -47,8 +47,77 @@ where
     }
 }
 
-// impl miette::Diagnostic for WeightedSelectorsError
-// "Selection requires at least one non-zero weight"
+impl<A, B> miette::Diagnostic for WeightedSelectorsError<A, B>
+where
+    A: miette::Diagnostic + 'static,
+    B: miette::Diagnostic + 'static,
+{
+    fn code<'a>(&'a self) -> Option<Box<dyn std::fmt::Display + 'a>> {
+        match self {
+            Self::A(a) => a.code(),
+            Self::B(b) => b.code(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn severity(&self) -> Option<miette::Severity> {
+        match self {
+            Self::A(a) => a.severity(),
+            Self::B(b) => b.severity(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn help<'a>(&'a self) -> Option<Box<dyn std::fmt::Display + 'a>> {
+        match self {
+            Self::A(a) => a.help(),
+            Self::B(b) => b.help(),
+            Self::ZeroWeightSum => {
+                Some(Box::new("Selection requires at least one non-zero weight"))
+            }
+        }
+    }
+
+    fn url<'a>(&'a self) -> Option<Box<dyn std::fmt::Display + 'a>> {
+        match self {
+            Self::A(a) => a.url(),
+            Self::B(b) => b.url(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn source_code(&self) -> Option<&dyn miette::SourceCode> {
+        match self {
+            Self::A(a) => a.source_code(),
+            Self::B(b) => b.source_code(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
+        match self {
+            Self::A(a) => a.labels(),
+            Self::B(b) => b.labels(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn related<'a>(&'a self) -> Option<Box<dyn Iterator<Item = &'a dyn miette::Diagnostic> + 'a>> {
+        match self {
+            Self::A(a) => a.related(),
+            Self::B(b) => b.related(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+
+    fn diagnostic_source(&self) -> Option<&dyn miette::Diagnostic> {
+        match self {
+            Self::A(a) => a.diagnostic_source(),
+            Self::B(b) => b.diagnostic_source(),
+            Self::ZeroWeightSum => None,
+        }
+    }
+}
 
 impl<P, A, B> Selector<P> for WeightedSelectorPair<A, B>
 where
