a565ae5c5133178954a38841d4dde7871f0c9ab5
Nic McPhee <mcphee@morris.umn.edu>
2024-11-17 12:33:33 -0600

    Add documentation and `Operator` impl for `&M` to `Mutator`

--- packages/ec-core/src/operator/mutator/mod.rs
+++ packages/ec-core/src/operator/mutator/mod.rs
@@ -2,7 +2,39 @@ use rand::rngs::ThreadRng;
 
 use super::{Composable, Operator};
 
+/// Mutate a given genome of type `G` generating a new
+/// genome of the same type.
+///
+/// # Examples
+///
+/// ```
+/// # use rand::thread_rng;
+/// # use rand::rngs::ThreadRng;
+/// # use rand::Rng;
+/// # use ec_core::operator::mutator::Mutator;
+/// #
+/// struct FlipOne;
+///
+/// impl Mutator<Vec<bool>> for FlipOne {
+///     fn mutate(&self, mut genome: Vec<bool>, rng: &mut ThreadRng) -> anyhow::Result<Vec<bool>> {
+///         let index = rng.gen_range(0..genome.len());
+///         genome[index] = !genome[index];
+///         Ok(genome)
+///     }
+/// }
+///
+/// let genome = vec![true, false, false, true];
+/// let child_genome = FlipOne.mutate(genome.clone(), &mut thread_rng()).unwrap();
+/// let num_diffs = genome
+///     .iter()
+///     .zip(child_genome.iter())
+///     .filter(|(x, y)| x != y)
+///     .count();
+/// assert_eq!(num_diffs, 1);
+/// ```
 pub trait Mutator<G> {
+    /// Mutate the given `genome`
+    ///
     /// # Errors
     /// This can return an error if there is an error mutating the given
     /// genome.
@@ -30,8 +62,19 @@ where
     type Output = G;
     type Error = anyhow::Error;
 
+    /// Apply this `Mutator` as an `Operator`
     fn apply(&self, genome: G, rng: &mut ThreadRng) -> Result<Self::Output, Self::Error> {
         self.mutator.mutate(genome, rng)
     }
 }
 impl<M> Composable for Mutate<M> {}
+
+/// Implement `Mutator` for a reference to a `Mutator`
+impl<M, G> Mutator<G> for &M
+where
+    M: Mutator<G>,
+{
+    fn mutate(&self, genome: G, rng: &mut ThreadRng) -> anyhow::Result<G> {
+        (**self).mutate(genome, rng)
+    }
+}
