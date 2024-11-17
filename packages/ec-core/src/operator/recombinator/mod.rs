4d278f532c2d63e7b835699a8f1b36c9be87bfb8
Nic McPhee <mcphee@morris.umn.edu>
2024-11-17 12:37:23 -0600

    Add documentation and `Operator` impl for `&M` to `Mutator`

--- packages/ec-core/src/operator/recombinator/mod.rs
+++ packages/ec-core/src/operator/recombinator/mod.rs
@@ -3,7 +3,7 @@ use rand::rngs::ThreadRng;
 use super::{Composable, Operator};
 
 /// Recombine (usually two or more) genomes into a new
-/// genome.
+/// genome of the same type
 ///
 /// GS: a generic for the _group_ of input genomes.
 /// Output: An associated type indicating what the result
@@ -12,6 +12,42 @@ use super::{Composable, Operator};
 /// Typically `Output` will be the same as the type of the
 /// individual genomes contained in `GS`, but that isn't captured
 /// (or required) here.
+///
+/// # Examples
+///
+/// ```
+/// # use rand::thread_rng;
+/// # use rand::rngs::ThreadRng;
+/// # use rand::Rng;
+/// # use ec_core::operator::recombinator::Recombinator;
+/// #
+/// struct SwapOne;
+/// type Parents = (Vec<u8>, Vec<u8>);
+///
+/// impl Recombinator<Parents> for SwapOne {
+///     type Output = Vec<u8>;
+///
+///     fn recombine(
+///         &self,
+///         (mut first_parent, second_parent): Parents,
+///         rng: &mut ThreadRng,
+///     ) -> anyhow::Result<Vec<u8>> {
+///         let index = rng.gen_range(0..first_parent.len());
+///         first_parent[index] = second_parent[index];
+///         Ok(first_parent)
+///     }
+/// }
+///
+/// let first_parent = vec![0, 0, 0, 0];
+/// let second_parent = vec![1, 1, 1, 1];
+/// let child = SwapOne
+///     .recombine((first_parent, second_parent), &mut thread_rng())
+///     .unwrap();
+/// let num_zeros = child.iter().filter(|&&x| x == 0).count();
+/// let num_ones = child.iter().filter(|&&x| x == 1).count();
+/// assert_eq!(num_zeros, 3);
+/// assert_eq!(num_ones, 1);
+/// ```
 pub trait Recombinator<GS> {
     type Output;
 
@@ -47,3 +83,15 @@ where
     }
 }
 impl<R> Composable for Recombine<R> {}
+
+/// Implement `Recombinator` for a reference to a `Recombinator`
+impl<R, GS> Recombinator<GS> for &R
+where
+    R: Recombinator<GS>,
+{
+    type Output = R::Output;
+
+    fn recombine(&self, genomes: GS, rng: &mut ThreadRng) -> anyhow::Result<Self::Output> {
+        (**self).recombine(genomes, rng)
+    }
+}
