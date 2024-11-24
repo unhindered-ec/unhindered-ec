95cea8ec4e2cc6361206017b06d4de7ece1f51ce
Nic McPhee <mcphee@morris.umn.edu>
2024-11-24 12:05:42 -0600

    Create `Constant` operator
    This returns a constant value regardless of the input value. This is essentially what `Identity`s implementation was, but now it is named correctly.

---
+++ packages/ec-core/src/operator/constant.rs
@@ -0,0 +1,26 @@
+use rand::rngs::ThreadRng;
+
+use super::{Composable, Operator};
+
+pub struct Constant<T> {
+    value: T,
+}
+
+impl<T> Constant<T> {
+    pub const fn new(value: T) -> Self {
+        Self { value }
+    }
+}
+
+impl<T> Operator<()> for Constant<T>
+where
+    T: Clone,
+{
+    type Output = T;
+    type Error = anyhow::Error;
+
+    fn apply(&self, (): (), _: &mut ThreadRng) -> Result<Self::Output, Self::Error> {
+        Ok(self.value.clone())
+    }
+}
+impl<T> Composable for Constant<T> {}
