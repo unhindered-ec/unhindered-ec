1a68f3f6c93658df4e943eb771a1ed5585db3bff
Nic McPhee <mcphee@morris.umn.edu>
2024-11-29 20:00:45 -0600

    Add documentation and tests to `Constant` and `Identity` operators

--- packages/ec-core/src/operator/identity.rs
+++ packages/ec-core/src/operator/identity.rs
@@ -4,6 +4,26 @@ use rand::rngs::ThreadRng;
 
 use super::{Composable, Operator};
 
+/// An [`Operator`] that always returns it's input value.
+///
+/// # See also
+///
+/// See [`Operator`] and [`Composable`].
+///
+/// # Examples
+///
+/// ```
+/// # use ec_core::operator::{Operator, identity::Identity};
+/// # use rand::thread_rng;
+/// #
+/// let mut rng = thread_rng();
+/// // This will always return the value that is passed in.
+/// let identity = Identity;
+///
+/// assert_eq!(identity.apply(3, &mut rng).unwrap(), 3);
+/// assert_eq!(identity.apply("string", &mut rng).unwrap(), "string");
+/// assert_eq!(identity.apply(Some(7), &mut rng).unwrap(), Some(7));
+/// ```
 pub struct Identity;
 
 impl<T> Operator<T> for Identity {
@@ -16,3 +36,21 @@ impl<T> Operator<T> for Identity {
 }
 
 impl Composable for Identity {}
+
+#[cfg(test)]
+mod tests {
+    use rand::thread_rng;
+
+    use crate::operator::{Operator, identity::Identity};
+
+    #[test]
+    fn is_constant() {
+        let mut rng = thread_rng();
+        // This should always return 5 regardless of the input.
+        let identity = Identity;
+
+        assert_eq!(identity.apply(3, &mut rng).unwrap(), 3);
+        assert_eq!(identity.apply("string", &mut rng).unwrap(), "string");
+        assert_eq!(identity.apply(Some(7), &mut rng).unwrap(), Some(7));
+    }
+}
