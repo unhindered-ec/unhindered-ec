acce51c3035fced182cec4bd5663ee8da8b2be5f
Justus Fluegel <justusfluegel@gmail.com>
2025-02-22 17:45:11 +0100

    Finish some more structs / enums
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/ec-core/src/operator/composable/then.rs
+++ packages/ec-core/src/operator/composable/then.rs
@@ -2,7 +2,26 @@ use rand::Rng;
 
 use super::{super::Operator, Composable};
 
-#[derive(Composable)]
+/// A composable [`Operator`] chaining two [`Operator`]'s
+///
+/// # Example
+/// ```
+/// # use ec_core::operator::{
+/// #     Operator,
+/// #     composable::{Map, Then, RepeatWith},
+/// #     constant::Constant,
+/// #     identity::Identity
+/// # };
+/// #
+/// let chained_operator = Then::new(
+///     RepeatWith::<_, 2>::new(Constant::new(1)),
+///     Map::new(Identity),
+/// );
+///
+/// assert_eq!(chained_operator.apply((), &mut rand::rng())?, [1; 2]);
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
+#[derive(Debug, Composable, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Then<F, G> {
     f: F,
     g: G,
@@ -14,7 +33,7 @@ impl<F, G> Then<F, G> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum ThenError<T, U> {
     First(T),
     Second(U),
@@ -24,10 +43,10 @@ impl<T, U> std::fmt::Display for ThenError<T, U> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::First(_) => f.write_str(
-                "Error while applying the first passed operator (`T`) in the `Then<T,>` Operator",
+                "Error while applying the first passed operator (`T`) in the `Then<T,_>` Operator",
             ),
             Self::Second(_) => f.write_str(
-                "Error while applying the second passed operator (`U`) in the `Then<,U>` Operator",
+                "Error while applying the second passed operator (`U`) in the `Then<_,U>` Operator",
             ),
         }
     }
