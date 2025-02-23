4321f635970ddf959237e0be40e1ad5306b6a4e6
Justus Fluegel <justusfluegel@gmail.com>
2025-02-23 18:52:29 +0100

    Add an example to the lexicase docs
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/ec-core/src/operator/selector/lexicase.rs
+++ packages/ec-core/src/operator/selector/lexicase.rs
@@ -19,6 +19,31 @@ use crate::{individual::Individual, population::Population, test_results::TestRe
 ///
 /// In case there are no test cases (i.e `num_test_cases` is zero) this devolves
 /// to a random selection.
+///
+/// # Examples
+/// ```
+/// # use ec_core::{
+/// #     individual::ec::EcIndividual,
+/// #     operator::selector::{Selector, lexicase::Lexicase},
+/// #     test_results::TestResults
+/// # };
+/// let population = [
+///     EcIndividual::new(100, TestResults::<i32>::from_iter([4, 10])),
+///     EcIndividual::new(100, TestResults::<i32>::from_iter([2, 10])),
+///     EcIndividual::new(100, TestResults::<i32>::from_iter([4, 5])),
+///     EcIndividual::new(100, TestResults::<i32>::from_iter([2, 5])),
+/// ];
+///
+/// let lexicase = Lexicase::new(2);
+///
+/// let selected = lexicase.select(&population, &mut rand::rng())?;
+///
+/// assert_eq!(
+///     selected,
+///     &EcIndividual::new(100, TestResults::from_iter([4, 10]))
+/// );
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Lexicase {
     num_test_cases: usize,
