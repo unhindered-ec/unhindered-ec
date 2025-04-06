5eb434262db0e6bf8dc7f1cfad4e5b45e98a264f
Nic McPhee <mcphee@morris.umn.edu>
2025-04-06 11:02:31 -0500

    Document `PrintChar` instruction
    This also implement `NumOpens`, which I forgot to do initially.

--- packages/push/src/instruction/printing/char.rs
+++ packages/push/src/instruction/printing/char.rs
@@ -1,12 +1,55 @@
 use std::io::Write;
 
 use super::super::{Instruction, instruction_error::PushInstructionError};
-use crate::{error::InstructionResult, push_vm::push_io::HasStdout};
+use crate::{error::InstructionResult, instruction::NumOpens, push_vm::push_io::HasStdout};
 
+/// An instruction that "prints" a single character.
+///
+/// # Inputs
+///
+/// The `PrintChar<CHAR>` instruction doesn't take any
+/// inputs because the character to print is specified in
+/// the generic parameter.
+///
+/// # Behavior
+///
+/// The `PrintChar<CHAR>` instruction "prints" a single character specified
+/// in the const generic for `PrintChar` to an internal buffer in the state.
+///
+/// ## Action Table
+///
+/// `PrintChar` doesn't affect any of the stacks.
+///
+/// # Examples
+///
+/// ```
+/// # use push::{
+/// #    instruction::{Instruction, printing::PrintChar},
+/// #    push_vm::{push_io::HasStdout, push_state::PushState},
+/// # };
+/// #
+/// // Build an initial state with empty stacks stack.
+/// let push_state = PushState::builder()
+///     .with_max_stack_size(0)
+///     .with_no_program()
+///     .with_instruction_step_limit(10)
+///     .build();
+/// // Print the character 'x'.
+/// let mut result = PrintChar::<'x'>::default().perform(push_state).unwrap();
+/// // Extract the printed output.
+/// let output = result.stdout_string().unwrap();
+/// // Assert that this is equal to "x".
+/// assert_eq!(output, "x");
+/// ```
+/// # Panics
+///
+/// This currently panics (due to an `unwrap()`) if attempting
+/// to write the character fails.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct PrintChar<const CHAR: char>;
 
 impl<const CHAR: char> PrintChar<CHAR> {
+    #[must_use]
     pub const fn new() -> Self {
         Self {}
     }
@@ -25,8 +68,15 @@ where
     }
 }
 
+impl<const CHAR: char> NumOpens for PrintChar<CHAR> {
+    fn num_opens(&self) -> usize {
+        0
+    }
+}
+
 pub type PrintSpace = PrintChar<' '>;
 pub type PrintNewline = PrintChar<'\n'>;
+pub type PrintPeriod = PrintChar<'.'>;
 
 #[cfg(test)]
 mod tests {
@@ -83,5 +133,18 @@ mod tests {
         let mut result = PrintNewline::default().perform(push_state).unwrap();
         let output = result.stdout_string().unwrap();
         assert_eq!(output, "\n");
+    }
+
+    #[test]
+    fn print_period() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+
+        let mut result = PrintPeriod::default().perform(push_state).unwrap();
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, ".");
     }
 }
