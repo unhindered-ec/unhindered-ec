e57b2886eda480cb66624964916c65a0ae9250e9
Nic McPhee <mcphee@morris.umn.edu>
2025-04-05 19:31:55 -0500

    Add tests for `print.rs`

--- packages/push/src/instruction/printing/print.rs
+++ packages/push/src/instruction/printing/print.rs
@@ -54,3 +54,141 @@ where
         Ok(state)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ordered_float::OrderedFloat;
+
+    use super::*;
+    use crate::push_vm::{push_state::PushState, stack::StackError};
+
+    #[test]
+    fn print_int() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_int_values([42])
+            .unwrap()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = Print::<i64>::default().perform(push_state).unwrap();
+        assert_eq!(result.stack::<i64>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "42");
+    }
+
+    #[test]
+    fn print_float() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_float_values([OrderedFloat(5.89)])
+            .unwrap()
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = Print::<OrderedFloat<f64>>::default()
+            .perform(push_state)
+            .unwrap();
+        assert_eq!(result.stack::<OrderedFloat<f64>>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "5.89");
+    }
+
+    #[test]
+    fn print_bool() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_bool_values([true])
+            .unwrap()
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = Print::<bool>::default().perform(push_state).unwrap();
+        assert_eq!(result.stack::<bool>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "true");
+    }
+
+    #[test]
+    fn print_underflow() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let result = Print::<i64>::default().perform(push_state).unwrap_err();
+        assert!(result.is_recoverable());
+        assert_eq!(
+            result.error(),
+            &PushInstructionError::StackError(StackError::Underflow {
+                num_requested: 1,
+                num_present: 0
+            })
+        );
+    }
+
+    #[test]
+    fn println_int() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_int_values([42])
+            .unwrap()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = PrintLn::<i64>::default().perform(push_state).unwrap();
+        assert_eq!(result.stack::<i64>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "42\n");
+    }
+
+    #[test]
+    fn println_float() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_float_values([OrderedFloat(5.89)])
+            .unwrap()
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = PrintLn::<OrderedFloat<f64>>::default()
+            .perform(push_state)
+            .unwrap();
+        assert_eq!(result.stack::<OrderedFloat<f64>>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "5.89\n");
+    }
+
+    #[test]
+    fn println_bool() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_bool_values([false])
+            .unwrap()
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let mut result = PrintLn::<bool>::default().perform(push_state).unwrap();
+        assert_eq!(result.stack::<bool>().size(), 0);
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "false\n");
+    }
+
+    #[test]
+    fn println_underflow() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+        let result = PrintLn::<i64>::default().perform(push_state).unwrap_err();
+        assert!(result.is_recoverable());
+        assert_eq!(
+            result.error(),
+            &PushInstructionError::StackError(StackError::Underflow {
+                num_requested: 1,
+                num_present: 0
+            })
+        );
+    }
+}
