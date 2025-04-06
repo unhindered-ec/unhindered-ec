089cdae4655c8ca27ce44680e210ee928466a0c1
Nic McPhee <mcphee@morris.umn.edu>
2025-04-05 19:42:00 -0500

    Add tests for `print_chars.rs`

--- packages/push/src/instruction/printing/print_char.rs
+++ packages/push/src/instruction/printing/print_char.rs
@@ -20,11 +20,68 @@ where
 
     fn perform(&self, mut state: State) -> InstructionResult<State, Self::Error> {
         // We need to remove this `unwrap()`.
-        writeln!(state.stdout(), "{CHAR}").unwrap();
+        write!(state.stdout(), "{CHAR}").unwrap();
         Ok(state)
     }
 }
 
 pub type PrintSpace = PrintChar<' '>;
+pub type PrintNewline = PrintChar<'\n'>;
 
-pub type PrintNewline = PrintChar<'n'>;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::push_vm::push_state::PushState;
+
+    #[test]
+    fn print_char_a() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+
+        let mut result = PrintChar::<'a'>.perform(push_state).unwrap();
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "a");
+    }
+
+    #[test]
+    fn print_char_newline() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+
+        let mut result = PrintChar::<'\n'>.perform(push_state).unwrap();
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "\n");
+    }
+
+    #[test]
+    fn print_space() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+
+        let mut result = PrintSpace::default().perform(push_state).unwrap();
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, " ");
+    }
+
+    #[test]
+    fn print_newline() {
+        let push_state = PushState::builder()
+            .with_max_stack_size(1)
+            .with_no_program()
+            .with_instruction_step_limit(10)
+            .build();
+
+        let mut result = PrintNewline::default().perform(push_state).unwrap();
+        let output = result.stdout_string().unwrap();
+        assert_eq!(output, "\n");
+    }
+}
