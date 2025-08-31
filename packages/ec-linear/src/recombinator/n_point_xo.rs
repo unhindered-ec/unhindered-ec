3a6002f40e91da8e7450b421229bee2dc06fe923
Nic McPhee <mcphee@morris.umn.edu>
2025-08-31 12:19:57 -0500

    Add N-point crossover operator
    Introduces a generic N-point crossover implementation allowing for a configurable number of crossover points. This operator randomly selects `N` points to alternately exchange segments between two parent genomes.
    
    Includes a test case demonstrating its behavior for a single crossover point.
    
    THIS IS ALMOST CERTAINLY NOT THE FINAL VERSION. At a minimum it needs more tests, and I don't think it will pass them consistently. If, for example, we choose 0 as a crossover point then I think we'll end up with the wrong number of segments.
    
    We're also going to do some timing studies to see how different approaches to this work from a performance perspective.

---
+++ packages/ec-linear/src/recombinator/n_point_xo.rs
@@ -0,0 +1,115 @@
+use std::iter::once;
+
+use ec_core::operator::recombinator::Recombinator;
+use itertools::Itertools;
+use rand::{Rng, seq::IteratorRandom};
+
+use super::{
+    crossover::Crossover,
+    errors::{CrossoverGeneError, DifferentGenomeLength},
+};
+use crate::genome::Linear;
+
+pub struct NPointXo<const N: usize>(());
+
+impl<const N: usize> NPointXo<N> {
+    #[must_use]
+    pub const fn new() -> Option<Self> {
+        if N == 0 {
+            return None;
+        }
+        Some(Self(()))
+    }
+}
+
+impl<G, const N: usize> Recombinator<[G; 2]> for NPointXo<N>
+where
+    G: Crossover + Linear,
+{
+    type Output = G;
+    type Error = CrossoverGeneError<G::SegmentCrossoverError>;
+
+    fn recombine<R: Rng + ?Sized>(
+        &self,
+        [mut first_genome, mut second_genome]: [G; 2],
+        rng: &mut R,
+    ) -> Result<Self::Output, Self::Error> {
+        let len = first_genome.size();
+        if len != second_genome.size() {
+            return Err(DifferentGenomeLength(len, second_genome.size()).into());
+        }
+
+        // let s = N + 2;
+        // let mut crossover_points = vec![0; s];
+
+        let mut crossover_points = [0; N];
+        (0..len).choose_multiple_fill(rng, &mut crossover_points);
+        crossover_points.sort_unstable();
+
+        // for crossover_point in crossover_points {
+        //     first_genome
+        //         .crossover_segment(&mut second_genome, crossover_point..len)
+        //         .map_err(CrossoverGeneError::Crossover)?;
+        // }
+
+        let segments = once(0).chain(crossover_points).chain(once(len)).chunks(2);
+        for mut segment in &segments {
+            let Some(start) = segment.next() else { break };
+            let Some(end) = segment.next() else { break };
+            // let start = segment.next().unwrap();
+            // let end = segment.next().unwrap();
+            first_genome
+                .crossover_segment(&mut second_genome, start..end)
+                .map_err(CrossoverGeneError::Crossover)?;
+        }
+
+        Ok(first_genome)
+    }
+}
+
+impl<G, const N: usize> Recombinator<(G, G)> for NPointXo<N>
+where
+    G: Crossover + Linear,
+{
+    type Output = G;
+    type Error = <Self as Recombinator<[G; 2]>>::Error;
+
+    fn recombine<R: Rng + ?Sized>(
+        &self,
+        genomes: (G, G),
+        rng: &mut R,
+    ) -> Result<Self::Output, Self::Error> {
+        self.recombine(<[G; 2]>::from(genomes), rng)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ec_core::{operator::recombinator::Recombinator, population::Population};
+
+    use crate::recombinator::n_point_xo::NPointXo;
+
+    #[test]
+    pub fn recombine_one_point() {
+        const GENOME_SIZE: usize = 10;
+
+        let first_parent = vec![0; GENOME_SIZE];
+        let second_parent = vec![1; GENOME_SIZE];
+
+        let crossover_operator = NPointXo::<1>::new().unwrap();
+
+        let mut rng = rand::rng();
+
+        let child = crossover_operator
+            .recombine([first_parent, second_parent], &mut rng)
+            .unwrap();
+
+        // Confirm that the child has the same size as the parents
+        assert_eq!(child.size(), GENOME_SIZE);
+
+        // Splits the child up into segments of equal values
+        let segments = child.chunk_by(|a, b| a == b);
+        // With one crossover point there should be exactly two segments.
+        assert_eq!(segments.count(), 2);
+    }
+}
