b0dc634eee5870d08c15fc874bc51d06d306bf90
Justus Flügel <justusfluegel@gmail.com>
2025-09-10 23:31:22 +0200

    Add very hacky implementation of floyds random sampling for uniform distinct values
    Signed-off-by: Justus Flügel <justusfluegel@gmail.com>

--- packages/ec-linear/benches/n_point_xo.rs
+++ packages/ec-linear/benches/n_point_xo.rs
@@ -13,7 +13,10 @@ mod n_point_xo {
     use divan::Bencher;
     use ec_core::operator::recombinator::Recombinator;
     use ec_linear::recombinator::{
-        n_point_xo::{NPointXoChunks, NPointXoMultiSwap, NPointXoPrimitive, NPointXoWindows},
+        n_point_xo::{
+            NPointXoChunks, NPointXoMultiSwap, NPointXoPrimitive, NPointXoPrimitiveFloyds,
+            NPointXoWindows,
+        },
         two_point_xo::TwoPointXo,
     };
 
@@ -88,6 +91,25 @@ mod n_point_xo {
                 let second_parent = vec![1; GENOME_SIZE];
 
                 let crossover_operator = NPointXoPrimitive::<N>::new().unwrap();
+
+                let rng = rand::rng();
+
+                ([first_parent, second_parent], crossover_operator, rng)
+            })
+            .bench_values(|(parents, crossover_operator, mut rng)| {
+                crossover_operator
+                    .recombine(divan::black_box(parents), &mut rng)
+                    .unwrap()
+            });
+    }
+    #[divan::bench(consts = NUM_CROSSOVERS)]
+    fn primitive_floyds<const N: usize>(bencher: Bencher) {
+        bencher
+            .with_inputs(|| {
+                let first_parent = vec![0; GENOME_SIZE];
+                let second_parent = vec![1; GENOME_SIZE];
+
+                let crossover_operator = NPointXoPrimitiveFloyds::<N>::new().unwrap();
 
                 let rng = rand::rng();
 
