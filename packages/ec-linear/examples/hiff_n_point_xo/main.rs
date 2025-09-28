b9ea493f42093bc9a7593959f56fb670eb84ee24
Nic McPhee <mcphee@morris.umn.edu>
2025-09-28 12:08:55 -0500

    Consolidate N-point crossover implementations
    Replace `NPointXo`, `NPointXoZero`, and `NPointXoStartEnd` with a single `NPointXo` operator that has similar or better performance than the other options we experimented with.
    
    Choose the general `sample_distinct_uniform_sorted` implementation since we found that being general here has a negligible performance impact. Removes the specialized implementations.
    
    Update benchmarks and examples to reflect the consolidated implementation, ensuring consistent behavior and proper testing for various N-point crossover scenarios.

--- packages/ec-linear/examples/hiff_n_point_xo/main.rs
+++ packages/ec-linear/examples/hiff_n_point_xo/main.rs
@@ -97,7 +97,7 @@ fn main() -> miette::Result<()> {
     let make_new_individual = Select::new(selector)
         .apply_twice()
         .then_map(GenomeExtractor)
-        .then(Recombine::new(NPointXo::<0>::new()))
+        .then(Recombine::new(NPointXo::<4>::new()))
         .then(Mutate::new(WithOneOverLength))
         .wrap::<GenomeScorer<_, _>>(scorer);
 
