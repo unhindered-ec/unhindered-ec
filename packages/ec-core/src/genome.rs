9290c0d7dacc0420fc1ba5794c96dee500ddffb0
Justus Fluegel <justusfluegel@gmail.com>
2025-12-08 16:52:16 +0100

    Document TestResults
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>
    
    Fix broken TestResults docs links
    
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>
    
    Further document TestResults
    
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/ec-core/src/genome.rs
+++ packages/ec-core/src/genome.rs
@@ -1,8 +1,10 @@
 pub trait Genome {
+    /// Type of each individual gene in this genome
     type Gene;
 }
 static_assertions::assert_obj_safe!(Genome<Gene = ()>);
 
 impl<T> Genome for Vec<T> {
+    /// For linear genomes (`Vec<T>`) each genome is of the type `T`.
     type Gene = T;
 }
