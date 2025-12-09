0195df4a3d65e1a672c55e60a2726cccab9599c6
Justus Fluegel <justusfluegel@gmail.com>
2025-12-09 13:47:20 +0100

    Document collection::Collection (Distribution) and untility methods
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/ec-core/src/operator/mod.rs
+++ packages/ec-core/src/operator/mod.rs
@@ -33,8 +33,9 @@ pub use composable::Composable;
 /// others.
 ///
 /// # Example
-/// This example shows how to implement a [`Operator`], not how you use
-/// predefined ones. For that, take a look at the [module-level
+/// This example shows how to implement a [`Operator`]  (in this case `Apply`,
+/// which applies an encapsulated unary function), not how you use predefined
+/// ones. For that, take a look at the [module-level
 /// documentation](super::operator)
 ///
 /// ```
@@ -79,6 +80,24 @@ pub trait Operator<Input>: Composable {
     /// [`Infallible`](std::convert::Infallible) if the operator does not error.
     type Error;
 
+    /// Apply this operator to an input
+    ///
+    /// This also takes an rng that is passed along to customize random number
+    /// generation behavior and avoid re-creating RNGs in each operator.
+    ///
+    /// # Example
+    /// ```
+    /// # use ec_core::operator::{constant::Constant, Operator};
+    /// # use rand::rng;
+    /// #
+    /// // A _constant_ operator that will always return the same value
+    /// // (in this case 5) regardless of its input
+    /// let my_constant_operator: Constant<_> = Constant::new(5);
+    ///
+    /// let Ok(sample_value) = my_constant_operator.apply((), &mut rng());
+    /// assert_eq!(sample_value, 5);
+    /// ```
+    ///
     /// # Errors
     /// This will return an error if there's some problem applying the operator.
     /// Given how general this concept is, there's no good way of saying here
