ab78d9634c6d41c75d27fcae2c193df4487617da
Justus Fluegel <justusfluegel@gmail.com>
2025-12-09 15:24:41 +0100

    Document genome
    Signed-off-by: Justus Fluegel <justusfluegel@gmail.com>

--- packages/ec-core/src/genome.rs
+++ packages/ec-core/src/genome.rs
@@ -1,3 +1,28 @@
+/// A genome for the ec system, made up of individual [`Genome::Gene`]'s.
+///
+/// Genomes are at the core of any ec system, since they are what all operations
+/// are operating on and what is scored to evaluate evolutionary progress.
+///
+/// Often you will be able to use a pre-defined genome like
+/// [`ec-linear::Bitstring`](#) or [`push::Plushy`](#).
+/// In certain circumstances, however, you may need to define your own genome
+/// specific to your problem domain. Take a look at the accompanying crates for
+/// different kinds of evolution for more examples of genome types.
+///
+/// # Example
+/// This is how you might implement a Bitstring genome type:
+/// ```
+/// # use ec_core::genome::Genome;
+/// # #[allow(dead_code)]
+/// struct PackedBitstring {
+///     length: usize,
+///     storage: Vec<u64>,
+/// }
+///
+/// impl Genome for PackedBitstring {
+///     type Gene = bool;
+/// }
+/// ```
 pub trait Genome {
     /// Type of each individual gene in this genome
     type Gene;
