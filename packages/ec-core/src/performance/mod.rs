09218076056e0149b899f1e1c8c1a58d87593564
Nic McPhee <mcphee@morris.umn.edu>
2026-01-10 14:24:57 -0600

    Expose `test_result` module
    Makes the `test_result` module public within the `ec-core` performance package.
    This enables its use and integration, supporting the ongoing restructuring of performance-related modules.

--- packages/ec-core/src/performance/mod.rs
+++ packages/ec-core/src/performance/mod.rs
@@ -5,7 +5,7 @@
 pub mod error_value;
 pub mod score_value;
 // I don't think we ever _use_ `TestResult` (singular), so maybe it goes away.
-//mod test_result;
+pub mod test_result;
 
 // TODO: Should there just be one struct (e.g., `Result<T>` with a `result: T`
 // field)   and then `Error` and `Score` should be traits that these structs can
