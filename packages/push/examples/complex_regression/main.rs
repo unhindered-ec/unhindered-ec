d6cc990eeed652f55616cce91698ea483fc40adc
Nic McPhee <mcphee@morris.umn.edu>
2026-05-30 18:11:14 -0500

    fix(examples): Dereference test_results.total() in examples

--- packages/push/examples/complex_regression/main.rs
+++ packages/push/examples/complex_regression/main.rs
@@ -184,7 +184,7 @@ fn main() -> miette::Result<()> {
         // max_generations-1.
         println!("Generation {generation_number:2} best is {best}");
 
-        if best.test_results.total() == &OrderedFloat(0.0f64) {
+        if *best.test_results.total() == OrderedFloat(0.0f64) {
             println!("SUCCESS");
             break;
         }
